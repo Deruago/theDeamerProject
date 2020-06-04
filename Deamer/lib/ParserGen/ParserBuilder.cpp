34e377e1cfc345cd54edc015ea9394e30a52dac7
Thimo Bohmer <thimo@bohmer.net>
2020-06-04 18:20:20 +0200

    Add GetOutput member function to retrieve output

--- Deamer/lib/ParserGen/ParserBuilder.cpp
+++ Deamer/lib/ParserGen/ParserBuilder.cpp
@@ -1,7 +1,13 @@
 #include "Deamer/LanguageGen/LanguageGenConstants.h"
 #include "Deamer/ParserGen/ParserBuilder.h"
+#include <string>
 
 ParserBuilder::ParserBuilder()
 {
 
+}
+
+std::string ParserBuilder::GetOutput()
+{
+    return ParserBuilder::Output;
 }
