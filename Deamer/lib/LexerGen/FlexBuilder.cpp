ab34dc280a9dbe1c9a273f8feba6ac58c97ba9f0
Thimo Bohmer <thimo@bohmer.net>
2020-06-04 18:18:17 +0200

    Add logic to actually build a flex file.

--- Deamer/lib/LexerGen/FlexBuilder.cpp
+++ Deamer/lib/LexerGen/FlexBuilder.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include "Deamer/LexerGen/FlexBuilder.h"
 
 FlexBuilder::FlexBuilder()
@@ -7,15 +8,32 @@ FlexBuilder::FlexBuilder()
 
 void FlexBuilder::AddNode(Node* node)
 {
+    std::ostringstream oss;
+    oss << node->TokenName << "   " << "(" << node->Regex << ")\n";
+    FlexBuilder::regexDeclarationPart += oss.str();
 
+    std::ostringstream oss2;
+    oss2 << "{" << node->TokenName << "}" << "   " << "{sscanf(yytext, \"%s\", yylval.name); return (" << node->TokenName << ");}\n";
+    FlexBuilder::tokenDeclarationPart += oss2.str();
 }
 
 bool FlexBuilder::FinishBuild()
 {
+    FlexBuilder::tokenDeclarationPart += "\n%%\n";
+
+    std::ostringstream oss;
+    oss << FlexBuilder::declarationPart << FlexBuilder::regexDeclarationPart << FlexBuilder::tokenDeclarationPart << FlexBuilder::functionPart;
+    FlexBuilder::Output = oss.str();
+
     return true;
 }
 
 bool FlexBuilder::StartBuild()
 {
+    std::ostringstream oss;
+    oss << "%{\n" << "#include <stdio.h>\n#include <string.h>\n#include \"./parser.tab.h\"\nvoid showError();\nextern int yylex();\n" << "%}\n" << "\n";
+    //oss << "%define IOSTREAM\n\n";
+    FlexBuilder::declarationPart = oss.str();
+    FlexBuilder::tokenDeclarationPart += "%%\n";
     return true;
 }
