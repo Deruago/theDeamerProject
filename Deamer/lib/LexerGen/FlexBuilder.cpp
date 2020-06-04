d8638bab2eb33ea2e36a280338ab8941716496f9
Thimo Bohmer <thimo@bohmer.net>
2020-06-04 18:24:38 +0200

    Removed unneccessary comment of FLexBuilder.cpp

--- Deamer/lib/LexerGen/FlexBuilder.cpp
+++ Deamer/lib/LexerGen/FlexBuilder.cpp
@@ -32,7 +32,6 @@ bool FlexBuilder::StartBuild()
 {
     std::ostringstream oss;
     oss << "%{\n" << "#include <stdio.h>\n#include <string.h>\n#include \"./parser.tab.h\"\nvoid showError();\nextern int yylex();\n" << "%}\n" << "\n";
-    //oss << "%define IOSTREAM\n\n";
     FlexBuilder::declarationPart = oss.str();
     FlexBuilder::tokenDeclarationPart += "%%\n";
     return true;
