a31dd150845fc9b3b3ec2f5112f847d519c04759
Thimo Bohmer <thimo@bohmer.net>
2020-06-06 17:12:58 +0200

    Add AstInformation class. This class is used to contain ast information

---
+++ Deamer/include/Deamer/AstGen/AstInformation.h
@@ -0,0 +1,15 @@
+#ifndef DEAMER_ASTGEN_ASTINFORMATION_H
+#define DEAMER_ASTGEN_ASTINFORMATION_H
+
+#include <string>
+
+class AstInformation
+{
+    public:
+        unsigned int LineNumber;
+        unsigned int CharNumber;
+        std::string  ValueName;
+        AstInformation(unsigned int lineNumber, unsigned int charNumber, std::string valueName);
+};
+
+#endif //DEAMER_ASTGEN_ASTINFORMATION_H
