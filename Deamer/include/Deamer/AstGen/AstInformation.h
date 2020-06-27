23a2804a00a56579055c2fa2496faa484950ddc0
Thimo Bohmer <thimo@bohmer.net>
2020-06-27 19:54:38 +0200

    Removed constructor of AstInformation struct

--- Deamer/include/Deamer/AstGen/AstInformation.h
+++ Deamer/include/Deamer/AstGen/AstInformation.h
@@ -9,7 +9,6 @@ struct AstInformation
         unsigned int LineNumber;
         unsigned int CharNumber;
         std::string  ValueName;
-        AstInformation(unsigned int lineNumber, unsigned int charNumber, std::string valueName);
 };
 
 #endif //DEAMER_ASTGEN_ASTINFORMATION_H
--- Deamer/lib/AstGen/AstInformation.cpp
+++ Deamer/lib/AstGen/AstInformation.cpp
@@ -1,8 +1,1 @@
 #include "Deamer/AstGen/AstInformation.h"
-
-AstInformation::AstInformation(unsigned int lineNumber, unsigned int charNumber, std::string valueName)
-{
-    AstInformation::LineNumber = lineNumber;
-    AstInformation::CharNumber = charNumber;
-    AstInformation::ValueName = valueName;
-}
