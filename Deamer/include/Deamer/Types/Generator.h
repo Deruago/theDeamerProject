e17a0c31a57e1593c9a4220e5d0e74210490b78f
Thimo Bohmer <thimo@bohmer.net>
2020-06-27 19:07:24 +0200

    Generator now supports functionality to check if directories exists, and to add directories

--- Deamer/include/Deamer/Types/Generator.h
+++ Deamer/include/Deamer/Types/Generator.h
@@ -13,8 +13,12 @@ class Generator
         Generator();
     public:
         virtual void DirTarget(std::string dirTarget) = 0;
+        virtual void FileTarget(std::string fileTarget) = 0;
         virtual bool Build();
         virtual bool Write();
+        bool DoesDirectoryExist(std::string* Directory);
+        bool CreateDirectory(std::string* Directory);
+        void CreateDirectoryIfNotExist(std::string* Directory);
 };
 
 #endif //DEAMER_TYPES_GENERATOR_H
--- Deamer/lib/Types/Generator.cpp
+++ Deamer/lib/Types/Generator.cpp
@@ -1,4 +1,7 @@
 #include "Deamer/Types/Generator.h"
+#include <filesystem>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 Generator::Generator()
 {
@@ -13,4 +16,37 @@ bool Generator::Build()
 bool Generator::Write()
 {
     return false;
+}
+
+bool Generator::DoesDirectoryExist(std::string* Directory)
+{
+    struct stat info;
+
+    if(stat(Directory->c_str(), &info ) != 0 )
+    {
+        return false;
+    }
+    else if(info.st_mode & S_IFDIR )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool Generator::CreateDirectory(std::string* Directory)
+{
+    int status;
+    status = mkdir(Directory->c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    return status;
+}
+
+void Generator::CreateDirectoryIfNotExist(std::string* Directory)
+{
+    if(!Generator::DoesDirectoryExist(Directory))
+    {
+        Generator::CreateDirectory(Directory);
+    }
 }
