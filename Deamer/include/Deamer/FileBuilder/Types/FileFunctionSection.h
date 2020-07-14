9f78e809f306241612b0212b26633a8163e543b3
Thimo Bohmer <thimo@bohmer.net>
2020-07-14 19:07:58 +0200

    Add FileFunctionSection class
    This class is ued for creating functions

--- Deamer/include/Deamer/FileBuilder/Types/FileFunctionSection.h
+++ Deamer/include/Deamer/FileBuilder/Types/FileFunctionSection.h
@@ -9,14 +9,24 @@
 #ifndef DEAMER_FILEBUILDER_TYPES_FILEFUNCTIONSECTION_H
 #define DEAMER_FILEBUILDER_TYPES_FILEFUNCTIONSECTION_H
 #include "Deamer/FileBuilder/FileSection.h"
+#include "Deamer/FileBuilder/Types/FileFunctionPrototypeSection.h"
+#include "Deamer/FileBuilder/Types/FileBlockSection.h"
 
 namespace deamer
 {
 	class FileFunctionSection : public FileSection
 	{
 		private:
+			FileFunctionPrototypeSection* functionPrototype;
 		protected:
 		public:
+			FileFunctionSection(FileFunctionPrototypeSection* functionPrototype);
+			FileFunctionSection(std::string functionName, std::vector<FileVariable*> functionArgs, FileVariableType* returnType, FileVariableType* scope);
+			FileFunctionSection(std::string functionName, std::vector<FileVariable*> functionArgs, FileVariableType* returnType, FileVariableType* scope, bool isMember);
+			~FileFunctionSection() = default;
+			void SetFunctionBlock(FileBlockSection* block); // Used to define the logic of the function
+			std::string GetOutput() override;
+			std::string GetFunctionPrototypeOutput(bool fullPath) const;
 	};
 }
 
---
+++ Deamer/lib/FileBuilder/Types/FileFunctionSection.cpp
@@ -0,0 +1,43 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Böhmer 2020
+ *
+ * Modified:
+ * -July 2020 Thimo Böhmer
+ */
+
+#include "Deamer/FileBuilder/Types/FileFunctionSection.h"
+
+deamer::FileFunctionSection::FileFunctionSection(FileFunctionPrototypeSection* functionPrototype)
+{
+	FileFunctionSection::functionPrototype = functionPrototype;
+}
+
+deamer::FileFunctionSection::FileFunctionSection(std::string functionName, std::vector<FileVariable*> functionArgs,
+	FileVariableType* returnType, FileVariableType* scope)
+{
+	functionPrototype = new FileFunctionPrototypeSection(functionName, functionArgs, returnType, scope);
+}
+
+deamer::FileFunctionSection::FileFunctionSection(std::string functionName, std::vector<FileVariable*> functionArgs,
+	FileVariableType* returnType, FileVariableType* scope, bool isMember)
+{
+	functionPrototype = new FileFunctionPrototypeSection(functionName, functionArgs, returnType, scope, isMember);
+}
+
+std::string deamer::FileFunctionSection::GetOutput()
+{
+	return "";
+}
+
+std::string deamer::FileFunctionSection::GetFunctionPrototypeOutput(bool fullPath) const
+{
+	if (fullPath)
+	{
+		return functionPrototype->GetOutput();
+	}
+	else
+	{
+		return functionPrototype->GetOutputWithoutFullDeclaration();
+	}
+}
