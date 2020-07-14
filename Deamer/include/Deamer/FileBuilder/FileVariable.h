3aac45af3f26f6cf869c2d6f3348de589f4d3679
Thimo Bohmer <thimo@bohmer.net>
2020-07-14 18:14:27 +0200

    Add FileVariable class
    This class is needed to define variables in the filebuilder.

---
+++ Deamer/include/Deamer/FileBuilder/FileVariable.h
@@ -0,0 +1,44 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Böhmer 2020
+ *
+ * Modified:
+ * -July 2020 Thimo Böhmer
+ */
+
+/*
+ * FileVariable is used to contain logic for variables.
+ * You can declare variables using the VariableType and VariableName.
+ * You can then use a FileInstructionSection to make instructions such as: b = a + c;
+ */
+
+#ifndef DEAMER_FILEBUILDER_FILEVARIABLE_H
+#define DEAMER_FILEBUILDER_FILEVARIABLE_H
+#include "Deamer/FileBuilder/FileVariableType.h"
+#include "Deamer/StringBuilder/StringBuilder.h"
+#include <string>
+
+namespace deamer
+{
+	class FileVariableType;
+	class StringBuilder;
+	
+	class FileVariable
+	{
+		private:
+			FileVariableType* variableType;
+			std::string variableName;
+			bool _is_member;
+
+			void AddOutputTypePath(StringBuilder* stringBuilder) const;
+			void AddVariableName(StringBuilder* stringBuilder) const;
+		protected:
+		public:
+			FileVariable(FileVariableType* varType, std::string varName, bool isMember);
+			~FileVariable() = default;
+			std::string GetOutput() const; // Returns full path. Thus including namespaces and classes. e.g int a, namespace_a::class_b c
+			std::string GetVariableName() const; // Returns only the variable name
+	};
+}
+
+#endif //DEAMER_FILEBUILDER_FILEVARIABLE_H
