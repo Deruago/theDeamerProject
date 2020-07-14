dd80293faf1865228a05adf31e566e640fae42a3
Thimo Bohmer <thimo@bohmer.net>
2020-07-14 19:15:06 +0200

    Add FileNamespaceSection class
    This class is used for creating namespaces.
    It has functionality to nest other strings in itself.
    Also you can easily embed other namespaces in itself

--- Deamer/include/Deamer/FileBuilder/Types/FileNamespaceSection.h
+++ Deamer/include/Deamer/FileBuilder/Types/FileNamespaceSection.h
@@ -9,14 +9,33 @@
 #ifndef DEAMER_FILEBUILDER_TYPES_FILENAMESPACESECTION_H
 #define DEAMER_FILEBUILDER_TYPES_FILENAMESPACESECTION_H
 #include "Deamer/FileBuilder/FileSection.h"
+#include "Deamer/FileBuilder/FileVariableType.h"
+#include "Deamer/StringBuilder/StringBuilder.h"
 
 namespace deamer
 {
-	class FileNamespaceSection : public FileSection
+	class StringBuilder;
+
+	class FileNamespaceSection : public FileSection, public FileVariableType
 	{
-	private:
-	protected:
-	public:
+		private:
+		protected:
+			std::string _namespaceName;
+			FileNamespaceSection* _baseNamespace;
+		
+			void AddNamespaceDeclaration(StringBuilder* beginDeclarations, StringBuilder* endDeclarations, unsigned* depth, unsigned indent, bool firstNamespace) const;
+			void AddEndingNamespaceDeclaration(StringBuilder* endDeclarations, unsigned indentSize, unsigned depth) const;
+			std::string GetThisNamespaceDeclarationWithoutNestedBaseNamespaceDeclarations() const;
+			void AddNamespaceTypeToStringBuilder(StringBuilder* stringBuilder, unsigned* depth, bool firstNamespace) const;
+			void AddFileSectionOutputToStringBuilder(StringBuilder* string_builder);
+		public:
+			FileNamespaceSection(std::string namespaceName);
+			FileNamespaceSection(FileNamespaceSection* baseNamespace, std::string namespaceName);
+			~FileNamespaceSection() = default;
+			std::string GetOutput() override;
+			std::string GetOutputType() const override;
+			std::string GetNamespaceName() const;
+			std::string EmbedThisString(std::string string_that_needs_to_be_embedded) const;
 	};
 }
 
---
+++ Deamer/lib/FileBuilder/Types/FileNamespaceSection.cpp
@@ -0,0 +1,116 @@
+#include "Deamer/FileBuilder/Types/FileNamespaceSection.h"
+
+deamer::FileNamespaceSection::FileNamespaceSection(std::string namespaceName)
+{
+	_namespaceName = namespaceName;
+}
+
+deamer::FileNamespaceSection::FileNamespaceSection(FileNamespaceSection* baseNamespace, std::string namespaceName) : FileNamespaceSection(namespaceName)
+{
+	_baseNamespace = baseNamespace;
+}
+
+std::string deamer::FileNamespaceSection::GetOutput()
+{
+	StringBuilder beginDeclaration;
+	StringBuilder midDeclaration;
+	StringBuilder endDeclaration;
+	unsigned depth = 0;
+	unsigned indentSize = 4;
+	AddNamespaceDeclaration(&beginDeclaration, &endDeclaration, &depth, indentSize, true);
+	AddFileSectionOutputToStringBuilder(&midDeclaration);
+	return beginDeclaration.GetOutput() + midDeclaration.GetOutput() + endDeclaration.GetOutput();
+}
+
+std::string deamer::FileNamespaceSection::GetOutputType() const
+{
+	StringBuilder stringBuilder;
+	unsigned depth = 0;
+	AddNamespaceTypeToStringBuilder(&stringBuilder, &depth, true);
+	return stringBuilder.GetOutputIgnoreNewLine();
+}
+
+std::string deamer::FileNamespaceSection::GetNamespaceName() const
+{
+	return _namespaceName;
+}
+
+std::string deamer::FileNamespaceSection::EmbedThisString(std::string string_that_needs_to_be_embedded) const
+{
+	StringBuilder beginDeclaration;
+	StringBuilder midDeclaration;
+	StringBuilder endDeclaration;
+	unsigned depth = 0;
+	unsigned indentSize = 4;
+	AddNamespaceDeclaration(&beginDeclaration, &endDeclaration, &depth, indentSize, true);
+	const std::string tmpReformattedString = midDeclaration.IndentEachLineInString(string_that_needs_to_be_embedded, indentSize, depth);
+	midDeclaration.Add(tmpReformattedString);
+
+	return beginDeclaration.GetOutput() + midDeclaration.GetOutput() + endDeclaration.GetOutput();
+}
+
+//std::string deamer::FileNamespaceSection::GetOutputUsingNamespace() const
+//{
+//	StringBuilder stringBuilder;
+//	stringBuilder.Add("using ");
+//	unsigned depth = 0;
+//	GetNamespaceUsing(&stringBuilder, &depth);
+//	return "";
+//}
+
+std::string deamer::FileNamespaceSection::GetThisNamespaceDeclarationWithoutNestedBaseNamespaceDeclarations() const
+{
+	StringBuilder stringBuilder;
+	stringBuilder.Add("namespace ");
+	stringBuilder.Add(_namespaceName);
+	return stringBuilder.GetOutputIgnoreNewLine();
+}
+
+void deamer::FileNamespaceSection::AddNamespaceTypeToStringBuilder(StringBuilder* stringBuilder, unsigned* depth, bool firstNamespace) const
+{
+	if (_baseNamespace != nullptr)
+	{
+		_baseNamespace->AddNamespaceTypeToStringBuilder(stringBuilder, depth, false);
+	}
+	
+	stringBuilder->Add(_namespaceName);
+	if (!firstNamespace)
+	{
+		stringBuilder->Add("::");
+	}
+	(*depth)++;
+}
+
+void deamer::FileNamespaceSection::AddFileSectionOutputToStringBuilder(StringBuilder* string_builder)
+{
+	for(FileSection* file_section : FileSections)
+	{
+		string_builder->Add(file_section->GetOutput());
+	}
+}
+
+void deamer::FileNamespaceSection::AddNamespaceDeclaration(StringBuilder* beginDeclarations,
+                                                           StringBuilder* endDeclarations, unsigned* depth, unsigned indent, bool firstNamespace) const
+{	
+	if(_baseNamespace != nullptr)
+	{
+		_baseNamespace->AddNamespaceDeclaration(beginDeclarations, endDeclarations, depth, indent, false);
+	}
+
+	beginDeclarations->Add(GetThisNamespaceDeclarationWithoutNestedBaseNamespaceDeclarations(), *depth, indent);
+	beginDeclarations->Add("{", *depth, indent);
+
+	if (firstNamespace)
+	{
+		AddEndingNamespaceDeclaration(endDeclarations, indent, *depth);
+	}
+	(*depth)++;
+}
+
+void deamer::FileNamespaceSection::AddEndingNamespaceDeclaration(StringBuilder* endDeclarations, unsigned indentSize, unsigned depth) const
+{
+	for(int i = depth; i > -1; i--)
+	{
+		endDeclarations->Add("}", indentSize, i);
+	}
+}
