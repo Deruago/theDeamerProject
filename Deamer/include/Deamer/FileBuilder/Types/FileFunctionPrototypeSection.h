fbc9fd5e2425a3b58ff7c62f213192c7010cf0a4
Thimo Bohmer <thimo@bohmer.net>
2020-07-14 19:07:29 +0200

    Add FileFunctionPrototypeSection class
    This class is used for defining function prototypes.

--- Deamer/include/Deamer/FileBuilder/Types/FileFunctionPrototypeSection.h
+++ Deamer/include/Deamer/FileBuilder/Types/FileFunctionPrototypeSection.h
@@ -9,16 +9,36 @@
 #ifndef DEAMER_FILEBUILDER_TYPES_FILEFUNCTIONPROTOTYPESECTION_H
 #define DEAMER_FILEBUILDER_TYPES_FILEFUNCTIONPROTOTYPESECTION_H
 #include "Deamer/FileBuilder/FileSection.h"
+#include "Deamer/FileBuilder/FileVariable.h"
+#include "Deamer/FileBuilder/FileVariableType.h"
 
 namespace deamer
 {
+	class StringBuilder;
+	class FileVariableType;
+	class FileVariable;
+
 	class FileFunctionPrototypeSection : public FileSection
 	{
 		private:
+			FileVariable* Function;
+			std::vector<FileVariable*> FunctionArguments;
+			FileVariableType* ReturnType;
+
+			void AddReturnType(StringBuilder* stringBuilder);
+			void AddFunctionName(StringBuilder* stringBuilder);
+			void AddFunctionArgument(StringBuilder* stringBuilder, FileVariable* argument);
+			void AddFunctionArguments(StringBuilder* stringBuilder);
+			void AddFunctionArgumentsToStringBuilder(StringBuilder* stringBuilder);
+			std::string GetFunctionName() const; // Returns only the function name
+			std::string GetFullFunctionName() const; // Returns the full path to the variable.
 		protected:
 		public:
-			FileFunctionPrototypeSection();
+			FileFunctionPrototypeSection(std::string functionName, std::vector<FileVariable*> functionArgs, FileVariableType* returnType, FileVariableType* scope);
+			FileFunctionPrototypeSection(std::string functionName, std::vector<FileVariable*> functionArgs, FileVariableType* returnType, FileVariableType* scope, bool isMember);
 			~FileFunctionPrototypeSection() = default;
+			std::string GetOutput() override;
+			std::string GetOutputWithoutFullDeclaration();
 	};
 }
 
---
+++ Deamer/lib/FileBuilder/Types/FileFunctionPrototypeSection.cpp
@@ -0,0 +1,89 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Böhmer 2020
+ *
+ * Modified:
+ * -July 2020 Thimo Böhmer
+ */
+
+#include "Deamer/FileBuilder/Types/FileFunctionPrototypeSection.h"
+#include "Deamer/StringBuilder/StringBuilder.h"
+
+deamer::FileFunctionPrototypeSection::FileFunctionPrototypeSection(std::string functionName,
+	std::vector<FileVariable*> functionArgs, FileVariableType* returnType, FileVariableType* scope) : FileFunctionPrototypeSection(functionName, functionArgs, returnType, scope, true)
+{
+}
+
+deamer::FileFunctionPrototypeSection::FileFunctionPrototypeSection(std::string functionName,
+	std::vector<FileVariable*> functionArgs, FileVariableType* returnType, FileVariableType* scope, bool isMember)
+{
+	FunctionArguments = functionArgs;
+	ReturnType = returnType;
+	Function = new FileVariable(scope, functionName, isMember);
+}
+
+std::string deamer::FileFunctionPrototypeSection::GetOutput()
+{
+	StringBuilder stringBuilder;
+	AddReturnType(&stringBuilder);
+	AddFunctionName(&stringBuilder);
+	AddFunctionArgumentsToStringBuilder(&stringBuilder);
+	return stringBuilder.GetOutputIgnoreNewLine();
+}
+
+std::string deamer::FileFunctionPrototypeSection::GetOutputWithoutFullDeclaration()
+{
+	StringBuilder stringBuilder;
+	AddReturnType(&stringBuilder);
+	stringBuilder.Add(GetFunctionName());
+	AddFunctionArgumentsToStringBuilder(&stringBuilder);
+	return stringBuilder.GetOutputIgnoreNewLine();
+}
+
+std::string deamer::FileFunctionPrototypeSection::GetFunctionName() const
+{
+	return Function->GetVariableName();
+}
+
+std::string deamer::FileFunctionPrototypeSection::GetFullFunctionName() const
+{
+	return Function->GetOutput();
+}
+
+void deamer::FileFunctionPrototypeSection::AddReturnType(StringBuilder* stringBuilder)
+{
+	stringBuilder->Add(ReturnType->GetOutputType());
+	stringBuilder->Add(" ");
+}
+
+void deamer::FileFunctionPrototypeSection::AddFunctionName(StringBuilder* stringBuilder)
+{
+	stringBuilder->Add(GetFullFunctionName());
+}
+
+void deamer::FileFunctionPrototypeSection::AddFunctionArgumentsToStringBuilder(StringBuilder* stringBuilder)
+{
+	stringBuilder->Add("(");
+	if (!FunctionArguments.empty())
+	{
+		AddFunctionArguments(stringBuilder);
+	}
+	stringBuilder->Add(")");
+}
+
+void deamer::FileFunctionPrototypeSection::AddFunctionArguments(StringBuilder* stringBuilder)
+{
+	for (unsigned int i = 0; i < FunctionArguments.size(); i++)
+	{
+		AddFunctionArgument(stringBuilder, FunctionArguments[i]);
+		if (i < FunctionArguments.size() - 1)
+		{
+			stringBuilder->Add(", ");
+		}
+	}
+}
+
+void deamer::FileFunctionPrototypeSection::AddFunctionArgument(StringBuilder* stringBuilder, FileVariable* argument)
+{
+	stringBuilder->Add(argument->GetOutput());
+}
