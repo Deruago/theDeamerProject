7e2da164b0d814b4ac37542440eeeed443cd8d75
Thimo Bohmer <thimo@bohmer.net>
2020-07-14 18:15:22 +0200

    Add class FileVariableType
    This class is used to define a variable type.
    For example namespace_a::clas_b or int.
    To define types you need to use this class as base class.

---
+++ Deamer/include/Deamer/FileBuilder/FileVariableType.h
@@ -0,0 +1,37 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Böhmer 2020
+ *
+ * Modified:
+ * -July 2020 Thimo Böhmer
+ */
+
+/*
+ * Is used to contain logic for FileVariableTypes.
+ * Types are basically things such as: int, string, char, std::vector<T>, namespace_a::class_b ect
+ * This class is abstract and is used as an interface. As types are classes it uses a FileClassSection
+ */
+
+#ifndef DEAMER_FILEBUILDER_FILEVARIABLETYPE_H
+#define DEAMER_FILEBUILDER_FILEVARIABLETYPE_H
+#include <string>
+
+namespace deamer
+{
+	class FileVariableType;
+
+	class FileVariableType
+	{
+		private:
+			FileVariableType* _variableTypeSection;
+		protected:
+		public:
+			FileVariableType(FileVariableType* variableTypeSection);
+			FileVariableType();
+			~FileVariableType() = default;
+			FileVariableType* GetFileVariableType() const;
+			virtual std::string GetOutputType() const = 0; //Outputs Variabletype. e.g deamer::filebuilder::class_a
+	};
+}
+
+#endif //DEAMER_FILEBUILDER_FILEVARIABLETYPE_H
