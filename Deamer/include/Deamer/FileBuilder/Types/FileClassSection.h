ee26c1ca57d17f5aa41a0682ce65ebb33969204c
Thimo Bohmer <thimo@bohmer.net>
2020-07-14 18:16:41 +0200

    Updated FileClassSection.h
    The FileClassSection class supports a lot more features.
    It can both output the definition and implementation of the class.
    It also supports functions to get members and add members.

--- Deamer/include/Deamer/FileBuilder/Types/FileClassSection.h
+++ Deamer/include/Deamer/FileBuilder/Types/FileClassSection.h
@@ -6,17 +6,100 @@
  * -July 2020 Thimo Böhmer
  */
 
+/*
+ * FileClassSection is used to define a class in c++.
+ * If you use the GetOutput function you get a valid Class.
+ */
+
 #ifndef DEAMER_FILEBUILDER_TYPES_FILECLASSSECTION_H
 #define DEAMER_FILEBUILDER_TYPES_FILECLASSSECTION_H
 #include "Deamer/FileBuilder/FileSection.h"
+#include "Deamer/FileBuilder/FileVariable.h"
+#include "Deamer/FileBuilder/FileVariableType.h"
+#include <vector>
+#include <string>
+
 
 namespace deamer
 {
-	class FileClassSection : public FileSection
+	class FileSection;
+	class FileNamespaceSection;
+	class FileVariable;
+	class FileVariableType;
+	class FileFunctionSection;
+	class StringBuilder;
+
+	class FileClassSection : public FileSection, public FileVariableType, public FileVariable
 	{
-	private:
-	protected:
-	public:
+		private:
+			FileNamespaceSection* _namespace;
+			std::vector<FileClassSection*> _superClasses;
+			std::vector<FileVariable*> privateMembers;
+			std::vector<FileVariable*> protectedMembers;
+			std::vector<FileVariable*> publicMembers;
+			std::vector<FileFunctionSection*> privateMemberFunctions;
+			std::vector<FileFunctionSection*> protectedMemberFunctions;
+			std::vector<FileFunctionSection*> publicMemberFunctions;
+		
+			void AddClass(StringBuilder* stringBuilder) const;
+			void AddNamespace(StringBuilder* stringBuilder) const;
+			void CreateClassSectionStringBuilder(std::string sectionType, StringBuilder* stringBuilder) const;
+			void AddPrivateMembersToStringBuilder(StringBuilder* stringBuilder) const;
+			void AddPrivateMemberFunctionsToStringBuilder(StringBuilder* stringBuilder) const;
+			void AddProtectedMembersToStringBuilder(StringBuilder* stringBuilder) const;
+			void AddProtectedMemberFunctionsToStringBuilder(StringBuilder* stringBuilder) const;
+			void AddPublicMembersToStringBuilder(StringBuilder* stringBuilder) const;
+			void AddPublicMemberFunctionsToStringBuilder(StringBuilder* stringBuilder) const;
+			void AddSuperClassForwardDeclarations(StringBuilder* stringBuilder) const;
+
+			// Needed for class declaration building
+			void AddClassBeginningDeclaration(StringBuilder* string_builder) const;
+			void AddSuperClassInheritanceToStringBuilder(StringBuilder* string_builder) const;
+			void AddPrivateSectionToStringBuilder(StringBuilder* string_builder) const;
+			void AddProtectedSectionToStringBuilder(StringBuilder* string_builder) const;
+			void AddPublicSectionToStringBuilder(StringBuilder* string_builder) const;
+			void AddClassEndDeclaration(StringBuilder* string_builder) const;
+		protected:
+		public:
+			FileClassSection(std::string className, FileNamespaceSection* scope); // If the class is a base class.
+			FileClassSection(std::string className, std::vector<FileClassSection*> superClasses, FileNamespaceSection* scope); // If the class is a sub class
+			~FileClassSection() = default;
+			std::string GetOutputDeclarationWithoutNamespace() const;
+			std::string GetOutputDeclaration() const;
+			std::string GetOutputImplementation() const;
+			std::string GetOutput() override; // Defaults to GetOutputDeclaration
+			std::string GetOutputType() const override; // Gives the output type. Including namespace
+			std::string GetForwardDeclaration() const;
+			std::string GetClassName() const;
+			std::string GetNamespace() const;
+
+			std::string GetPrivateSection() const;
+			std::string GetProtectedSection() const;
+			std::string GetPublicSection() const;
+		
+			std::vector<FileSection*> GetPrivateMembers() const;
+			std::vector<FileFunctionSection*> GetPrivateMemberFunctions() const;
+			std::vector<FileVariable*> GetPrivateMemberVariables() const;
+
+			std::vector<FileSection*> GetProtectedMembers() const;
+			std::vector<FileFunctionSection*> GetProtectedMemberFunctions() const;
+			std::vector<FileVariable*> GetProtectedMemberVariables() const;
+			
+			std::vector<FileSection*> GetPublicMembers() const;
+			std::vector<FileFunctionSection*> GetPublicMemberFunctions() const;
+			std::vector<FileVariable*> GetPublicMemberVariables() const;
+
+			void AddPrivateMember(FileVariable* member);
+			void AddProtectedMember(FileVariable* member);
+			void AddPublicMember(FileVariable* member);
+
+			void AddPrivateMember(FileFunctionSection* memberFunction);
+			void AddProtectedMember(FileFunctionSection* memberFunction);
+			void AddPublicMember(FileFunctionSection* memberFunction);
+
+			void AddPrivateMember(std::string functionName, std::vector<FileVariable*> args, FileVariableType* returnType);
+			void AddProtectedMember(std::string functionName, std::vector<FileVariable*> args, FileVariableType* returnType);
+			void AddPublicMember(std::string functionName, std::vector<FileVariable*> args, FileVariableType* returnType);
 	};
 }
 
