25ab3f125b019867a969d1171210b181ac72fc1c
Thimo Bohmer <thimo@bohmer.net>
2020-07-23 21:20:17 +0200

    Add BisonTokenFormatter class
    This class is used to abstract the construction
    of Types in bison

---
+++ Deamer/include/Deamer/ParserGen/BisonBuilder/BisonTokenFormatter.h
@@ -0,0 +1,22 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Bohmer 2020
+ *
+ * Modified:
+ * -July 2020 Thimo Bohmer
+ */
+
+#ifndef DEAMER_PARSERGEN_BISONBUILDER_BISONTOKENFORMATTER_H
+#define DEAMER_PARSERGEN_BISONBUILDER_BISONTOKENFORMATTER_H
+
+namespace deamer
+{
+	class BisonTokenFormatter
+	{
+	public:
+		BisonTokenFormatter() = default;
+		~BisonTokenFormatter() = default;
+	};
+}
+
+#endif //DEAMER_PARSERGEN_BISONBUILDER_BISONTOKENFORMATTER_H
---
+++ Deamer/lib/ParserGen/BisonBuilder/BisonTokenFormatter.cpp
@@ -0,0 +1,1 @@
+#include "Deamer/ParserGen/BisonBuilder/BisonTokenFormatter.h"
