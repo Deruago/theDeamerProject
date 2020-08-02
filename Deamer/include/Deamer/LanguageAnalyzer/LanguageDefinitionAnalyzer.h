cd456918f6e7f8629b0d5dfe79087529ac458a71
Thimo Bohmer <thimo@bohmer.net>
2020-08-02 20:51:55 +0200

    Add class languageDefinitionAnalyzer class
    This class will be able to give useful metadata
    about the language definition. For example it can
    output specific trees and later be able to use a visitor
    pattern to let other languagedefinition specific classes
    function on the tree.

---
+++ Deamer/include/Deamer/LanguageAnalyzer/LanguageDefinitionAnalyzer.h
@@ -0,0 +1,16 @@
+#ifndef DEAMER_LANGUAGEANALYZER_LANGUAGEDEFINITIONANALYZER_H
+#define DEAMER_LANGUAGEANALYZER_LANGUAGEDEFINITIONANALYZER_H
+
+namespace deamer
+{
+	class LanguageDefinitionAnalyzer
+	{
+	public:
+		LanguageDefinitionAnalyzer() = default;
+		~LanguageDefinitionAnalyzer() = default;
+
+		
+	};
+}
+
+#endif //DEAMER_LANGUAGEANALYZER_LANGUAGEDEFINITIONANALYZER_H
---
+++ Deamer/lib/LanguageAnalyzer/LanguageDefinitionAnalyzer.cpp
@@ -0,0 +1,2 @@
+#include "Deamer/LanguageAnalyzer/LanguageDefinitionAnalyzer.h"
+
