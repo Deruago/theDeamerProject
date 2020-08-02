7f95ca141d62cd041061fc905dc8d0e4c1708d62
Thimo Bohmer <thimo@bohmer.net>
2020-08-02 20:56:15 +0200

    Moved BitwiseEnum class implementation
    Moved the implementation to the header file instead.
    Also added some alias functions. e.g has_flag.

--- Deamer/include/Deamer/Types/BitwiseEnum.h
+++ Deamer/include/Deamer/Types/BitwiseEnum.h
@@ -41,14 +41,176 @@ public:
 	bool operator!=(T b);
 
 	void set_flag(T a);
+	void set_flag(T a, bool cond);
 	void clear_flag(T a);
 	void set_flags(std::vector<T> a_vector);
 	void clear_flags(std::vector<T> a_vector);
 	
 	bool is_flag_set(T a);
 	bool is_flag_not_set(T a);
+	bool has_flag(T a);
+	bool has_flags(T a);
 	bool are_flags_set(std::vector<T> a_vector);
 	bool are_flags_not_set(std::vector<T> a_vector);
 };
+
+template <class T>
+unsigned BitwiseEnum<T>::operator()() const
+{
+	return bitwise_value;
+}
+
+template <class T>
+void BitwiseEnum<T>::operator=(T b)
+{
+	bitwise_value = static_cast<unsigned>(b);
+}
+
+template <class T>
+void BitwiseEnum<T>::operator|=(T b)
+{
+	bitwise_value = operator|(b);
+}
+
+template <class T>
+void BitwiseEnum<T>::operator&=(T b)
+{
+	bitwise_value = operator&(b);
+}
+
+template <class T>
+void BitwiseEnum<T>::operator^=(T b)
+{
+	bitwise_value = operator^(b);
+}
+
+template <class T>
+unsigned BitwiseEnum<T>::operator&(T b)
+{
+	return bitwise_value & static_cast<unsigned>(b);
+}
+
+template <class T>
+unsigned BitwiseEnum<T>::operator^(T b)
+{
+	return bitwise_value ^ static_cast<unsigned>(b);
+}
+
+template <class T>
+unsigned BitwiseEnum<T>::operator|(T b)
+{
+	return bitwise_value | static_cast<unsigned>(b);
+}
+
+template <class T>
+bool BitwiseEnum<T>::operator==(std::vector<T> b_vector)
+{
+	for (T b : b_vector)
+	{
+		if (operator!=(b))
+			return false;
+	}
+	return true;
+}
+
+template <class T>
+bool BitwiseEnum<T>::operator!=(std::vector<T> b_vector)
+{
+	for (T b : b_vector)
+	{
+		if (operator==(b))
+			return false;
+	}
+	return true;
+}
+
+template <class T>
+bool BitwiseEnum<T>::operator==(T b)
+{
+	return bitwise_value & static_cast<unsigned>(b);
+}
+
+template <class T>
+bool BitwiseEnum<T>::operator!=(T b)
+{
+	return ~bitwise_value & static_cast<unsigned>(b);
+}
+
+template <class T>
+unsigned BitwiseEnum<T>::value() const
+{
+	return bitwise_value;
+}
+
+template <class T>
+void BitwiseEnum<T>::set_flag(T a)
+{
+	bitwise_value = operator|(a);
+}
+
+template <class T>
+void BitwiseEnum<T>::set_flag(T a, bool cond)
+{
+	if (cond)
+		set_flag(a);
+	else
+		clear_flag(a);
+}
+
+template <class T>
+void BitwiseEnum<T>::clear_flag(T a)
+{
+	bitwise_value &= ~static_cast<unsigned>(a);
+}
+
+template <class T>
+void BitwiseEnum<T>::set_flags(std::vector<T> a_vector)
+{
+	for (T a : a_vector)
+		set_flag(a);
+}
+
+template <class T>
+void BitwiseEnum<T>::clear_flags(std::vector<T> a_vector)
+{
+	for (T a : a_vector)
+		clear_flag(a);
+}
+
+template <class T>
+bool BitwiseEnum<T>::is_flag_set(T a)
+{
+	return operator==(a);
+}
+
+template <class T>
+bool BitwiseEnum<T>::is_flag_not_set(T a)
+{
+	return operator!=(a);
+}
+
+template <class T>
+bool BitwiseEnum<T>::has_flag(T a)
+{
+	return is_flag_set(a);
+}
+
+template <class T>
+bool BitwiseEnum<T>::has_flags(T a)
+{
+	return are_flags_set(a);
+}
+
+template <class T>
+bool BitwiseEnum<T>::are_flags_set(std::vector<T> a_vector)
+{
+	return operator==(a_vector);
+}
+
+template <class T>
+bool BitwiseEnum<T>::are_flags_not_set(std::vector<T> a_vector)
+{
+	return operator!=(a_vector);
+}
 
 #endif //DEAMER_TYPES_BITWISEENUM_H
