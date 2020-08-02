b9f871ee82917aa69898900f02bf7ed5ccb06d3b
Thimo Bohmer <thimo@bohmer.net>
2020-08-02 20:57:43 +0200

    Updated enums to be compatible with bitwise enum
    The enums now use powers of 2 as their values.

--- Deamer/include/Deamer/LanguageGen/LanguageGenConstants.h
+++ Deamer/include/Deamer/LanguageGen/LanguageGenConstants.h
@@ -29,11 +29,41 @@ namespace deamer
 	// It is recommended to let analysers or dedicated functions handle this.
 	enum class RuleType_t
     {
-    	empty,
-    	standard,
-    	grouped,
-    	vectorised
+		none = 0,
+    	empty = 1,
+    	standard = 2,
+    	grouped = 4,
+    	vectorised = 8,
+		recursive = 16,
+		all = 32,
     };
+
+	// This enum is used to define what kind of type a certain type is.
+	// e.g we have types that are used for grouping types, start types and standard types
+	enum class TokenType_t
+	{
+		none = 0,
+		standard = 1,
+		vector = 2,
+		start = 4,
+		grouped = 8,
+		all = 16,
+	};
+
+	enum class TokenPermission_t
+	{
+		none = 0,
+		node = 1,
+		ast = 2,
+		ignore = 4,
+		all = 8,
+	};
+
+	enum class TypeContinuation_t
+	{
+		recursive,
+		vector,
+	};
 }
 
 #endif //DEAMER_LANGUAGEGEN_LANGUAGEGEN_CONSTANTS_H
