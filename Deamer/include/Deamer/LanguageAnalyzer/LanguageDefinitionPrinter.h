4bf27a77a87f8f7e9660469a9deadb4c16f4241d
Thimo Bohmer <thimo@bohmer.net>
2020-08-02 20:53:47 +0200

    add LanguageDefinitionPrinter class
    This class is used to print the languageDefinition
    in different ways. It is used to give the user easy access
    to displaying its grammar in the console.

---
+++ Deamer/include/Deamer/LanguageAnalyzer/LanguageDefinitionPrinter.h
@@ -0,0 +1,33 @@
+#ifndef DEAMER_LANGUAGEANALYZER_LANGUAGEDEFINITIONPRINTER_H
+#define DEAMER_LANGUAGEANALYZER_LANGUAGEDEFINITIONPRINTER_H
+#include "Deamer/AstGen/AstBuilder.h"
+#include "Deamer/ParserGen/BisonBuilder/BisonRuleSectionFormatter.h"
+
+namespace deamer
+{
+	class LanguageDefinition;
+
+	class LanguageDefinitionPrinter
+	{
+	public:
+		LanguageDefinitionPrinter() = default;
+		~LanguageDefinitionPrinter() = default;
+
+		void PrintCompleteToken(Token* token, unsigned indent) const;
+		void PrintCompleteRule(std::vector<Type*>& visited_types, Rule* rule, unsigned indent) const;
+		void PrintCompleteType(Type* type, unsigned indent) const;
+		void PrintTree(LanguageDefinition& language_definition) const;
+		bool TypeAlreadyVisited(const std::vector<Type*>& visited_types, Token* new_type) const;
+		std::string MakeIndent(unsigned indent) const;
+		std::string MakeIndentation(unsigned depth, unsigned indent) const;
+		void PrintCompleteType(std::vector<Type*> visited_types, Type* type, unsigned indent) const;
+		void PrintAllTokens(LanguageDefinition& language_definition) const;
+		void PrintAllRules(LanguageDefinition& language_definition) const;
+		void PrintAllNodes(LanguageDefinition& language_definition) const;
+		void PrintAllGroupedTypes(LanguageDefinition& language_definition) const;
+		void PrintAllIgnoredNodes(LanguageDefinition& language_definition) const;
+		void PrintAllIgnoredTypes(LanguageDefinition& language_definition) const;
+	};
+}
+
+#endif //DEAMER_LANGUAGEANALYZER_LANGUAGEDEFINITIONPRINTER_H
