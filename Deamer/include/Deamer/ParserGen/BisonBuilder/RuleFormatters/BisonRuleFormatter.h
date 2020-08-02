598bffe108aa5388cf4e1637613b5ac401223f6c
Thimo Bohmer <thimo@bohmer.net>
2020-08-02 21:33:25 +0200

    Update BisonRuleFormatter
    Old references to "rule" (used in function arguments)
    are replaced with the field CurrentRule
    
    Add logic to support correct usage of vectorised tokens

--- Deamer/include/Deamer/ParserGen/BisonBuilder/RuleFormatters/BisonRuleFormatter.h
+++ Deamer/include/Deamer/ParserGen/BisonBuilder/RuleFormatters/BisonRuleFormatter.h
@@ -21,38 +21,44 @@ namespace deamer
 	class BisonRuleFormatter
 	{
 	private:
-		std::string MakeRuleProductionDefinition(Rule* rule) const;
-		virtual std::string MakeExecutedCodeForProductionRule(Rule* rule) const = 0;
+		std::string MakeRuleProductionDefinition() const;
+		virtual std::string MakeExecutedCodeForProductionRule() const = 0;
 
 		std::string MakeBeginExecutedCodePartForProductionRule() const;
 		std::string MakeEndExecutedCodePartForProductionRule() const;
 	protected:
 		std::string LanguageName = "";
 		unsigned CurrentLineNumber;
-		bool IsFirstType;
-		Type CurrentType = Type("", false, false);
+		Type* CurrentType = nullptr;
+		Rule* CurrentRule;
+
 		bool CurrentTokenIsNode(Token* CurrentToken) const;
 		bool CurrentTokenHasAnAstNode(Token* CurrentToken) const;
 		bool CurrentTypeIsTheFirstType() const;
 		std::string MakeArgumentReferenceVariable(int& ArgumentCount) const;
 		std::string MakeArgument(const std::string& LanguageName, int& ArgumentCount, Token* CurrentToken) const;
-		std::string AddFirstArgument(const std::string& LanguageName, Rule* rule, int& j) const;
+		std::string AddFirstArgument(const std::string& LanguageName, int& j) const;
 		std::string MakeNewAstNode(const std::string& LanguageName, int ArgumentCount,
 		                           Token* CurrentToken) const;
-		std::string AddRestOfAllArguments(const std::string& LanguageName, Rule* rule, int ArgumentCount) const;
+		std::string AddRestOfAllArguments(const std::string& LanguageName, int ArgumentCount) const;
 		std::string MakeAstType(const std::string& LanguageName) const;
-		std::string AddArgumentsToAstType(const std::string& LanguageName, Rule* rule) const;
+		std::string AddArgumentsToAstType(const std::string& LanguageName) const;
 		std::string MakeAstTree(const std::string& LanguageName) const;
+		std::string MakeAddToMainTypeLine() const;
+		unsigned GetRecursiveTypeLocationInRule() const;
+		std::string MakeVectorCreationLogic() const;
+		std::string TypeToClassName(Type* type) const;
+		std::string MakeEndAddToMainTypeLine() const;
 		std::string MakeAstNode(const std::string& LanguageName) const;
 		std::string MakeAstNode(const std::string& LanguageName, const std::string& TokenName) const;
 		std::string MakeOutputCodeForAstTree() const;
 		std::string MakeOutputCodeWhenNodeIsStartType() const;
 		std::string MakeProductionRuleNotation() const;
-		std::string FormatRule(Rule* rule) const;
+		std::string FormatRule() const;
 	public:
-		BisonRuleFormatter(std::string& languageName, unsigned currentLineNumber, bool isFirstType, Type& currentType);
+		BisonRuleFormatter(std::string& languageName, unsigned currentLineNumber, bool isFirstType, Type* currentType, Rule* currentRule);
 		virtual ~BisonRuleFormatter() = default;
-		std::string MakeRule(Rule* rule) const;
+		std::string MakeRule() const;
 		const char EndProductionRule() const;
 		const char* MakeIndentation() const;
 	};
--- Deamer/lib/ParserGen/BisonBuilder/RuleFormatters/BisonRuleFormatter.cpp
+++ Deamer/lib/ParserGen/BisonBuilder/RuleFormatters/BisonRuleFormatter.cpp
@@ -13,34 +13,34 @@
 #include <string>
 #include <sstream>
 
-deamer::BisonRuleFormatter::BisonRuleFormatter(std::string& languageName, unsigned currentLineNumber, bool isFirstType, Type& currentType)
+deamer::BisonRuleFormatter::BisonRuleFormatter(std::string& languageName, unsigned currentLineNumber, bool isFirstType, Type* currentType, Rule* currentRule)
 {
 	LanguageName = languageName;
 	CurrentLineNumber = currentLineNumber;
-	IsFirstType = isFirstType;
 	CurrentType = currentType;
+	CurrentRule = currentRule;
 }
 
-std::string deamer::BisonRuleFormatter::MakeRule(Rule* rule) const
+std::string deamer::BisonRuleFormatter::MakeRule() const
 {
-	return FormatRule(rule);
+	return FormatRule();
 }
 
 
-std::string deamer::BisonRuleFormatter::FormatRule(Rule* rule) const
+std::string deamer::BisonRuleFormatter::FormatRule() const
 {
 	std::string newProductionRuleText = MakeIndentation() + MakeProductionRuleNotation() +
-		MakeRuleProductionDefinition(rule) + MakeBeginExecutedCodePartForProductionRule() + MakeExecutedCodeForProductionRule(rule) + MakeEndExecutedCodePartForProductionRule() + EndProductionRule();
+		MakeRuleProductionDefinition() + MakeBeginExecutedCodePartForProductionRule() + MakeExecutedCodeForProductionRule() + MakeEndExecutedCodePartForProductionRule() + EndProductionRule();
 	return newProductionRuleText;
 }
 
-std::string deamer::BisonRuleFormatter::MakeRuleProductionDefinition(Rule* rule) const
+std::string deamer::BisonRuleFormatter::MakeRuleProductionDefinition() const
 {
 	std::string tmpString;
-	for (int i = 0; i < rule->Tokens.size(); i++)
+	for (int i = 0; i < CurrentRule->Tokens.size(); i++)
 	{
-		tmpString += rule->Tokens[i]->TokenName;
-		if (i < rule->Tokens.size())
+		tmpString += CurrentRule->Tokens[i]->TokenName;
+		if (i < CurrentRule->Tokens.size())
 		{
 			tmpString += ' ';
 		}
@@ -78,17 +78,17 @@ const char deamer::BisonRuleFormatter::EndProductionRule() const
 
 bool deamer::BisonRuleFormatter::CurrentTokenIsNode(Token* CurrentToken) const
 {
-	return CurrentToken->IsNode;
+	return CurrentToken->TokenPermission.has_flag(TokenPermission_t::node);
 }
 
 bool deamer::BisonRuleFormatter::CurrentTokenHasAnAstNode(Token* CurrentToken) const
 {
-	return CurrentToken->CreateAst;
+	return CurrentToken->TokenPermission.has_flag(TokenPermission_t::ast);
 }
 
 bool deamer::BisonRuleFormatter::CurrentTypeIsTheFirstType() const
 {
-	return IsFirstType;
+	return CurrentType->TokenType.has_flag(TokenType_t::start);
 }
 
 std::string deamer::BisonRuleFormatter::MakeArgumentReferenceVariable(int& ArgumentCount) const
@@ -98,24 +98,30 @@ std::string deamer::BisonRuleFormatter::MakeArgumentReferenceVariable(int& Argum
 
 std::string deamer::BisonRuleFormatter::MakeArgument(const std::string& LanguageName, int& ArgumentCount, Token* CurrentToken) const
 {
+	std::string argument;
+	//argument += "{";
 	if (CurrentTokenIsNode(CurrentToken))
 	{
-		return MakeNewAstNode(LanguageName, ArgumentCount, CurrentToken);
+		argument += MakeNewAstNode(LanguageName, ArgumentCount, CurrentToken);
 	}
 	else
 	{
-		return MakeArgumentReferenceVariable(ArgumentCount);
+		argument += MakeArgumentReferenceVariable(ArgumentCount);
 	}
+	//argument += "}";
+	return argument;
 }
 
-std::string deamer::BisonRuleFormatter::AddFirstArgument(const std::string& LanguageName, Rule* rule, int& j) const
+std::string deamer::BisonRuleFormatter::AddFirstArgument(const std::string& LanguageName, int& j) const
 {
 	std::string tmpString;
 	j = 0;
 	bool createdFirstParam = false;
-	while (j < rule->Tokens.size() && !createdFirstParam)
+	while (j < CurrentRule->Tokens.size() && !createdFirstParam)
 	{
-		Token* CurrentToken = rule->Tokens[j];
+		Token* CurrentToken = CurrentRule->Tokens[j];
+		if (CurrentToken == CurrentType)
+			continue;
 		if (CurrentTokenHasAnAstNode(CurrentToken))
 		{
 			tmpString += MakeArgument(LanguageName, j, CurrentToken);
@@ -132,12 +138,14 @@ std::string deamer::BisonRuleFormatter::MakeNewAstNode(const std::string& Langua
 	return "new " + LanguageName + "::" + "AstNode_" + CurrentToken->TokenName + "($" + std::to_string(ArgumentCount + 1) + ")";
 }
 
-std::string deamer::BisonRuleFormatter::AddRestOfAllArguments(const std::string& LanguageName, Rule* rule, int ArgumentCount) const
+std::string deamer::BisonRuleFormatter::AddRestOfAllArguments(const std::string& LanguageName, int ArgumentCount) const
 {
 	std::string tmpString;
-	for (;ArgumentCount < rule->Tokens.size(); ArgumentCount++)
+	for (;ArgumentCount < CurrentRule->Tokens.size(); ArgumentCount++)
 	{
-		Token* CurrentToken = rule->Tokens[ArgumentCount];
+		Token* CurrentToken = CurrentRule->Tokens[ArgumentCount];
+		if (CurrentToken == CurrentType)
+			continue;
 		if (!CurrentTokenHasAnAstNode(CurrentToken))
 		{
 			continue;
@@ -161,26 +169,72 @@ std::string deamer::BisonRuleFormatter::MakeAstType(const std::string& LanguageN
 	}
 }
 
-std::string deamer::BisonRuleFormatter::AddArgumentsToAstType(const std::string& LanguageName, Rule* rule) const
+std::string deamer::BisonRuleFormatter::AddArgumentsToAstType(const std::string& LanguageName) const
 {
 	int argumentCount;
-	return "({" +AddFirstArgument(LanguageName, rule, argumentCount) + AddRestOfAllArguments(LanguageName, rule, argumentCount) + "});\n";
+	return "(" + AddFirstArgument(LanguageName, argumentCount) + AddRestOfAllArguments(LanguageName, argumentCount) + ")" + MakeEndAddToMainTypeLine() + ";\n";
 }
-
 
 std::string deamer::BisonRuleFormatter::MakeAstTree(const std::string& LanguageName) const
 {
-	return  "      " + LanguageName + "::" + "AstTree_" + CurrentType.TokenName + "* ASTTREE_" + CurrentType.TokenName + " = new " + LanguageName + "::" + "AstTree_" + CurrentType.TokenName;
+	return  "      " + LanguageName + "::" + "AstTree_" + CurrentType->TokenName + "* ASTTREE_" + CurrentType->TokenName + " = new " + LanguageName + "::" + "AstTree_" + CurrentType->TokenName;
+}
+
+std::string deamer::BisonRuleFormatter::MakeAddToMainTypeLine() const
+{
+	if (CurrentType->TokenType.has_flag(TokenType_t::vector))
+		return MakeVectorCreationLogic() + "      $$->push_back(new ";
+	else
+		return "      $$ = new ";
+}
+
+std::string deamer::BisonRuleFormatter::MakeVectorCreationLogic() const
+{
+	std::string vectorised_check_allocation;
+	if (CurrentRule->RuleType.has_flag(RuleType_t::recursive))
+	{
+		vectorised_check_allocation += "      $$ = $" + std::to_string(GetRecursiveTypeLocationInRule()) + ";\n";
+	}
+	else
+	{
+		vectorised_check_allocation += "      $$ = new std::vector<" + TypeToClassName(CurrentType) + "*>();\n";
+	}
+	return vectorised_check_allocation;
+}
+
+unsigned deamer::BisonRuleFormatter::GetRecursiveTypeLocationInRule() const
+{
+	unsigned argument_place_in_rule = 1;
+	for (Token* token : CurrentRule->Tokens)
+	{
+		if (token == CurrentType)
+			return argument_place_in_rule;
+		argument_place_in_rule++;
+	}
+	return argument_place_in_rule;
+}
+
+std::string deamer::BisonRuleFormatter::TypeToClassName(Type* type) const
+{
+	return LanguageName + "::AstNode_" + type->TokenName;
+}
+
+std::string deamer::BisonRuleFormatter::MakeEndAddToMainTypeLine() const
+{
+	if (CurrentType->TokenType.has_flag(TokenType_t::vector))
+		return ")";
+	else
+		return "";
 }
 
 std::string deamer::BisonRuleFormatter::MakeAstNode(const std::string& LanguageName) const
 {
-	return + "      $$ = new " + LanguageName + "::" + "AstNode_" + CurrentType.TokenName;
+	return MakeAddToMainTypeLine() + LanguageName + "::" + "AstNode_" + CurrentType->TokenName;
 }
 
 std::string deamer::BisonRuleFormatter::MakeAstNode(const std::string& LanguageName, const std::string& TokenName) const
 {
-	return +"      $$ = new " + LanguageName + "::" + "AstNode_" + TokenName;
+	return MakeAddToMainTypeLine() + LanguageName + "::" + "AstNode_" + TokenName;
 }
 
 std::string deamer::BisonRuleFormatter::MakeOutputCodeWhenNodeIsStartType() const
@@ -194,5 +248,5 @@ std::string deamer::BisonRuleFormatter::MakeOutputCodeWhenNodeIsStartType() cons
 
 std::string deamer::BisonRuleFormatter::MakeOutputCodeForAstTree() const
 {
-	return "      ASTTREE_" + CurrentType.TokenName + "->SetCurrentTree(ASTTREE_" + CurrentType.TokenName + ");\n";
+	return "      ASTTREE_" + CurrentType->TokenName + "->SetCurrentTree(ASTTREE_" + CurrentType->TokenName + ");\n";
 }
