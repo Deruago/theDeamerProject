86854bdb4f431d8d65c22fde14bc86d1c372915e
Thimo Bohmer <thimo@bohmer.net>
2020-08-09 16:44:27 +0200

    Add AstNodeFormatter class
    This class is used to format a set of astnodes

---
+++ Deamer/include/Deamer/AstGen/AstNodeFormatter.h
@@ -0,0 +1,24 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Böhmer 2020
+ *
+ * Modified:
+ * -August 2020 Thimo Böhmer
+ */
+
+#ifndef DEAMER_ASTGEN_ASTNODEFORMATTER_H
+#define DEAMER_ASTGEN_ASTNODEFORMATTER_H
+
+namespace deamer
+{
+	class AstNodeFormatter
+	{
+	private:
+	protected:
+	public:
+		AstNodeFormatter() = default;
+		~AstNodeFormatter() = default;
+	};
+}
+
+#endif //DEAMER_ASTGEN_ASTNODEFORMATTER_H
