457ffba55c4ea3056fc1c09ed6c77a1e8889d01f
Thimo Bohmer <thimo@bohmer.net>
2020-08-10 16:36:53 +0200

    Created implementation files for AstFileFormatter classes

---
+++ Deamer/lib/AstGen/AstFormatter/AstFileFormatter.cpp
@@ -0,0 +1,9 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Böhmer 2020
+ *
+ * Modified:
+ * -August 2020 Thimo Böhmer
+ */
+
+#include "Deamer/AstGen/AstFormatter/AstFileFormatter.h"
