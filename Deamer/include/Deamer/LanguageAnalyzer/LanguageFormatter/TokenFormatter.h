c5a0a1d7ad7b8fe2e81bcb29661338d6c6951983
Thimo Bohmer <thimo@bohmer.net>
2020-08-10 16:39:44 +0200

    Made TokenFormatter class declaration

--- Deamer/include/Deamer/LanguageAnalyzer/LanguageFormatter/TokenFormatter.h
+++ Deamer/include/Deamer/LanguageAnalyzer/LanguageFormatter/TokenFormatter.h
@@ -8,6 +8,7 @@
 
 #ifndef DEAMER_LANGUAGEANALYZER_LANGUAGEFORMATTER_TOKENFORMATTER_H
 #define DEAMER_LANGUAGEANALYZER_LANGUAGEFORMATTER_TOKENFORMATTER_H
+#include <string>
 
 
 namespace deamer
@@ -20,7 +21,11 @@ namespace deamer
 		TokenFormatter() = default;
 		~TokenFormatter() = default;
 
-
+		std::string MakeFunctionArgument();
+		std::string MakeTypeCallAsClassField();
+		std::string MakeTypeAsCtorInputVariable();
+		std::string MakeConstructorTypeAssignment();
+		std::string MakeTypeAsClassField();
 	};
 }
 
--- Deamer/lib/LanguageAnalyzer/LanguageFormatter/TokenFormatter.cpp
+++ Deamer/lib/LanguageAnalyzer/LanguageFormatter/TokenFormatter.cpp
@@ -7,3 +7,28 @@
  */
 
 #include "Deamer/LanguageAnalyzer/LanguageFormatter/TokenFormatter.h"
+
+#include <string>
+
+#include "Deamer/LanguageGen/LanguageDefinitionDataStructures/Type.h"
+
+
+std::string deamer::TokenFormatter::MakeFunctionArgument()
+{
+}
+
+std::string deamer::TokenFormatter::MakeTypeCallAsClassField()
+{
+}
+
+std::string deamer::TokenFormatter::MakeTypeAsCtorInputVariable()
+{
+}
+
+std::string deamer::TokenFormatter::MakeConstructorTypeAssignment()
+{
+}
+
+std::string deamer::TokenFormatter::MakeTypeAsClassField()
+{
+}
