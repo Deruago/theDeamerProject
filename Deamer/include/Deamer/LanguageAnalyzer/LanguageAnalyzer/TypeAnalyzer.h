4154fcee4f011ffe7d2e5ea258920112dc88ca6a
Thimo Bohmer <thimo@bohmer.net>
2020-08-19 15:56:00 +0200

    Add member function GetVectorOfMinimalAmountOfTokensUsedDefiningThisType
    This can be used to get a vector with all unique types,
    that are used to define the type.

--- Deamer/include/Deamer/LanguageAnalyzer/LanguageAnalyzer/TypeAnalyzer.h
+++ Deamer/include/Deamer/LanguageAnalyzer/LanguageAnalyzer/TypeAnalyzer.h
@@ -9,23 +9,26 @@
 #ifndef DEAMER_LANGUAGEANALYZER_LANGUAGEANALYZER_TYPEANALYZER_H
 #define DEAMER_LANGUAGEANALYZER_LANGUAGEANALYZER_TYPEANALYZER_H
 
+#include "Deamer/LanguageGen/LanguageDefinitionDataStructures/Type.h"
 #include <vector>
-
-#include "Deamer/ParserGen/BisonBuilder/RuleFormatters/BisonEmptyRuleFormatter.h"
 
 namespace deamer
 {
 	class Token;
+	struct TokenAppearance;
 	
 	class TypeAnalyzer
 	{
 	private:
+		Type type_;
 		void AddTokenToVectorIfNotAlreadyInVector(std::vector<Token*>& tokens, Token* token) const;
 	public:
-		TypeAnalyzer() = default;
+		TypeAnalyzer(Type& type);
 		~TypeAnalyzer() = default;
 
-		std::vector<Token*> GetVectorOfUniqueTokensDefiningThisType(Type& type) const;
+		std::vector<Token*> GetVectorOfUniqueTokensDefiningThisType() const;
+		// i.e: t: a a, a a a returns vector with a a a. as a a a is minimal amount of tokens that are needed to cover all rules.
+		std::vector<deamer::TokenAppearance> GetVectorOfMinimalAmountOfTokensUsedDefiningThisType() const;
 	};
 }
 
--- Deamer/lib/LanguageAnalyzer/LanguageAnalyzer/TypeAnalyzer.cpp
+++ Deamer/lib/LanguageAnalyzer/LanguageAnalyzer/TypeAnalyzer.cpp
@@ -9,17 +9,53 @@
 #include "Deamer/LanguageAnalyzer/LanguageAnalyzer/TypeAnalyzer.h"
 #include "Deamer/LanguageGen/LanguageDefinitionDataStructures/Token.h"
 #include "Deamer/LanguageGen/LanguageDefinitionDataStructures/Type.h"
+#include "Deamer/LanguageAnalyzer/LanguageAnalyzer/Datastructures/TokenAppearance.h"
 #include "Deamer/LanguageGen/LanguageGenConstants.h"
 #include <vector>
+#include <map>
 
-std::vector<deamer::Token*> deamer::TypeAnalyzer::GetVectorOfUniqueTokensDefiningThisType(Type& type) const
+deamer::TypeAnalyzer::TypeAnalyzer(Type& type)
+{
+    type_ = type;
+}
+
+std::vector<deamer::Token*> deamer::TypeAnalyzer::GetVectorOfUniqueTokensDefiningThisType() const
 {
     std::vector<Token*> token_vector;
-    for (Rule* rule : type.Rules)
+    for (Rule* rule : type_.Rules)
         for (Token* token : rule->Tokens)
             if (!token->TokenPermission.has_flag(TokenPermission_t::ignore))
                 AddTokenToVectorIfNotAlreadyInVector(token_vector, token);
     return token_vector;
+}
+
+std::vector<deamer::TokenAppearance> deamer::TypeAnalyzer::GetVectorOfMinimalAmountOfTokensUsedDefiningThisType() const
+{
+    std::map<Token*, unsigned> count_token;
+    for (Rule* rule : type_.Rules)
+    {
+        std::map<Token*, unsigned> tmp_token_count;
+        std::vector<Token*> tmp_token_vector;
+        for (Token* token : rule->Tokens)
+        {
+	        const unsigned new_token_count = tmp_token_count[token] + 1;
+            if (new_token_count == 1)
+	        	tmp_token_vector.push_back(token);
+            tmp_token_count[token] = new_token_count;
+        }
+    	for(Token* token_in_tmp_vector : tmp_token_vector)
+    	{
+            if (count_token[token_in_tmp_vector] < tmp_token_count[token_in_tmp_vector])
+                count_token[token_in_tmp_vector] = tmp_token_count[token_in_tmp_vector];
+    	}
+    }
+    std::vector<TokenAppearance> token_appearances;
+	for(Token* token : GetVectorOfUniqueTokensDefiningThisType())
+	{
+        TokenAppearance token_appearance = { token, count_token[token]};
+        token_appearances.push_back(token_appearance);
+	}
+    return token_appearances;
 }
 
 void deamer::TypeAnalyzer::AddTokenToVectorIfNotAlreadyInVector(std::vector<Token*>& tokens, Token* token) const
