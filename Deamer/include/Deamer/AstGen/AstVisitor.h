40f2d9debe8cd7a07e2e3df51cc11d3457a2fe08
Thimo Bohmer <thimo@bohmer.net>
2020-08-19 16:10:18 +0200

    Add abstract class AstVisitor
    This class is used by generated AstVisitors, to
    guarantee an dispatch member function for an AstNode

---
+++ Deamer/include/Deamer/AstGen/AstVisitor.h
@@ -0,0 +1,28 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Böhmer 2020
+ *
+ * Modified:
+ * -August 2020 Thimo Böhmer
+ */
+
+#ifndef DEAMER_ASTGEN_ASTVISITOR_H
+#define DEAMER_ASTGEN_ASTVISITOR_H
+#include "AstNode.h"
+
+/*
+  * This is the base AstVisitor. It uses 1 virtual function that accepts an AstNode.
+  * This virtual function should be overriden by subclasses to integrate their own specific functionality.
+ */
+
+namespace deamer
+{
+	class AstVisitor
+    {
+    public:
+        AstVisitor() = default;
+        virtual void Dispatch(AstNode& astnode) const;
+    };
+}
+
+#endif //DEAMER_ASTGEN_ASTVISITOR_H
---
+++ Deamer/lib/AstGen/AstVisitor.cpp
@@ -0,0 +1,13 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Böhmer 2020
+ *
+ * Modified:
+ * -August 2020 Thimo Böhmer
+ */
+
+#include "Deamer/AstGen/AstVisitor.h"
+
+void deamer::AstVisitor::Dispatch(AstNode& astnode) const
+{
+}
