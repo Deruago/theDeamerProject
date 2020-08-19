2818d7da9a7b7a7d5aed1dfda5ee08f22c7d0943
Thimo Bohmer <thimo@bohmer.net>
2020-08-19 16:24:29 +0200

    Add AstBuilderType
    This enum is used to define the types of generatable
    astnode types. (i.e interpreter and visitor)

---
+++ Deamer/include/Deamer/AstGen/AstNodeDataStructures/AstBuilderType.h
@@ -0,0 +1,21 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Böhmer 2020
+ *
+ * Modified:
+ * -August 2020 Thimo Böhmer
+ */
+
+#ifndef DEAMER_ASTGEN_ASTNODEDATASTRUCTURES_ASTBUILDERTYPE_H
+#define DEAMER_ASTGEN_ASTNODEDATASTRUCTURES_ASTBUILDERTYPE_H
+
+namespace deamer
+{
+	enum class AstBuilderType
+	{
+		interpreter,
+		visitor,
+	};
+}
+
+#endif //DEAMER_ASTGEN_ASTNODEDATASTRUCTURES_ASTBUILDERTYPE_H
