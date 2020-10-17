bf45d0c2433bd8fdb738875882f646427993277f
Thimo Bohmer <thimo@bohmer.net>
2020-10-17 17:07:56 +0200

    Update TypeAnalyzer
    It can now return if a type contains empty rules,
    And analyze what kind of token types are used in defining the current type.

--- Deamer/include/Deamer/LanguageAnalyzer/LanguageAnalyzer/TypeAnalyzer.h
+++ Deamer/include/Deamer/LanguageAnalyzer/LanguageAnalyzer/TypeAnalyzer.h
@@ -26,14 +26,20 @@ namespace deamer
 		Rule* CreateRule_Without_Ignored_Tokens(Rule* rule) const;
 		bool IsThisRuleUniqueInVector(const std::vector<Rule*>& rules, const Rule& rule) const;
 		bool AreRulesEqual(const Rule& rule1, const Rule& rule2) const;
+
 	public:
 		TypeAnalyzer(Type& type);
 		~TypeAnalyzer() = default;
 
+		bool HasTypeEmptyRule() const;
+		
 		std::vector<Token*> GetVectorOfUniqueTokensDefiningThisType() const;
 		// i.e: t: a a, a a a returns vector with a a a. as a a a is minimal amount of tokens that are needed to cover all rules.
 		std::vector<deamer::TokenAppearance> GetVectorOfMinimalAmountOfTokensUsedDefiningThisType() const;
 		std::vector<Rule*> GetVectorOfUniqueRulesApplyingIgnoredTokens() const;
+
+		unsigned GetTokenTypesOccuringInType() const;
+		unsigned GetTokenTypesOccuringInType(const Type& type, BitwiseEnum<TokenType_t>& tokenType, BitwiseEnum<TokenPermission_t>& tokenPermission) const;
 	};
 }
 
--- Deamer/lib/LanguageAnalyzer/LanguageAnalyzer/TypeAnalyzer.cpp
+++ Deamer/lib/LanguageAnalyzer/LanguageAnalyzer/TypeAnalyzer.cpp
@@ -14,112 +14,182 @@
 #include "Deamer/LanguageGen/RuleFactory.h"
 #include <vector>
 #include <map>
-#include <iostream>
 
 deamer::TypeAnalyzer::TypeAnalyzer(Type& type)
 {
-    type_ = &type;
+	type_ = &type;
+}
+
+bool deamer::TypeAnalyzer::HasTypeEmptyRule() const
+{
+	for (Rule* rule : type_->Rules)
+	{
+		if (rule->RuleType.has_flag(RuleType_t::empty))
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
 std::vector<deamer::Token*> deamer::TypeAnalyzer::GetVectorOfUniqueTokensDefiningThisType() const
 {
-    std::vector<Token*> token_vector;
-    for (Rule* rule : type_->Rules)
-        for (Token* token : rule->Tokens)
-            if (!token->TokenPermission.has_flag(TokenPermission_t::ignore) && type_ != token)
-                AddTokenToVectorIfNotAlreadyInVector(token_vector, token);
-    return token_vector;
+	std::vector<Token*> token_vector;
+	for (Rule* rule : type_->Rules)
+		for (Token* token : rule->Tokens)
+			if (!token->TokenPermission.has_flag(TokenPermission_t::ignore) && type_ != token)
+				AddTokenToVectorIfNotAlreadyInVector(token_vector, token);
+	return token_vector;
 }
 
 std::vector<deamer::TokenAppearance> deamer::TypeAnalyzer::GetVectorOfMinimalAmountOfTokensUsedDefiningThisType() const
 {
-    std::map<Token*, unsigned> count_token;
-    for (Rule* rule : type_->Rules)
-    {
-        std::map<Token*, unsigned> tmp_token_count;
-        std::vector<Token*> tmp_token_vector;
-        for (Token* token : rule->Tokens)
-        {
-        	if (type_ == token)
-                continue;
-	        const unsigned new_token_count = tmp_token_count[token] + 1;
-            if (new_token_count == 1)
-	        	tmp_token_vector.push_back(token);
-            tmp_token_count[token] = new_token_count;
-        }
-    	for(Token* token_in_tmp_vector : tmp_token_vector)
-    	{
-            if (count_token[token_in_tmp_vector] < tmp_token_count[token_in_tmp_vector])
-                count_token[token_in_tmp_vector] = tmp_token_count[token_in_tmp_vector];
-    	}
-    }
-    std::vector<TokenAppearance> token_appearances;
-	for(Token* token : GetVectorOfUniqueTokensDefiningThisType())
+	std::map<Token*, unsigned> count_token;
+	for (Rule* rule : type_->Rules)
 	{
-        TokenAppearance token_appearance = { token, count_token[token]};
-        token_appearances.push_back(token_appearance);
+		std::map<Token*, unsigned> tmp_token_count;
+		std::vector<Token*> tmp_token_vector;
+		for (Token* token : rule->Tokens)
+		{
+			if (type_ == token)
+				continue;
+			const unsigned new_token_count = tmp_token_count[token] + 1;
+			if (new_token_count == 1)
+				tmp_token_vector.push_back(token);
+			tmp_token_count[token] = new_token_count;
+		}
+		for (Token* token_in_tmp_vector : tmp_token_vector)
+		{
+			if (count_token[token_in_tmp_vector] < tmp_token_count[token_in_tmp_vector])
+				count_token[token_in_tmp_vector] = tmp_token_count[token_in_tmp_vector];
+		}
 	}
-    return token_appearances;
+	std::vector<TokenAppearance> token_appearances;
+	for (Token* token : GetVectorOfUniqueTokensDefiningThisType())
+	{
+		TokenAppearance token_appearance = { token, count_token[token] };
+		token_appearances.push_back(token_appearance);
+	}
+	return token_appearances;
 }
 
 std::vector<deamer::Rule*> deamer::TypeAnalyzer::GetVectorOfUniqueRulesApplyingIgnoredTokens() const
 {
-    std::vector<Rule*> unique_rules_without_ignored_tokens;
-	for(Rule* tmp_rule : type_->Rules)
+	std::vector<Rule*> unique_rules_without_ignored_tokens;
+	for (Rule* tmp_rule : type_->Rules)
 	{
-        auto new_rule = CreateRule_Without_Ignored_Tokens(tmp_rule);
-        if(IsThisRuleUniqueInVector(unique_rules_without_ignored_tokens, *new_rule))
-            unique_rules_without_ignored_tokens.push_back(new_rule);
+		auto new_rule = CreateRule_Without_Ignored_Tokens(tmp_rule);
+		if (IsThisRuleUniqueInVector(unique_rules_without_ignored_tokens, *new_rule))
+			unique_rules_without_ignored_tokens.push_back(new_rule);
 	}
-	
-    return unique_rules_without_ignored_tokens;
+
+	return unique_rules_without_ignored_tokens;
+}
+
+unsigned deamer::TypeAnalyzer::GetTokenTypesOccuringInType() const
+{
+	BitwiseEnum<TokenType_t> tokenTypesEncountered;
+	BitwiseEnum<TokenPermission_t> tokenPermissionsEncountered;
+
+	return GetTokenTypesOccuringInType(*type_, tokenTypesEncountered, tokenPermissionsEncountered);;
+}
+
+unsigned deamer::TypeAnalyzer::GetTokenTypesOccuringInType(const Type& type, BitwiseEnum<TokenType_t>& tokenType, BitwiseEnum<TokenPermission_t>& tokenPermission) const
+{
+	BitwiseEnum<TokenType_t> tokenTypesEncountered;
+	BitwiseEnum<TokenPermission_t> tokenPermissionsEncountered;
+	for (const Rule* rule : type.Rules)
+	{
+		for (const Token* token : rule->Tokens)
+		{
+			if (token->TokenType.has_flag(TokenType_t::grouped))
+			{
+				GetTokenTypesOccuringInType(*static_cast<const Type*>(token), tokenTypesEncountered, tokenPermissionsEncountered);
+			}
+			else
+			{
+				if (token->TokenType.has_flag(TokenType_t::standard))
+				{
+					// As some types are subsets of higher subsets we need to check which is activated
+					if (token->TokenType.has_flag(TokenType_t::vector))
+					{
+						tokenTypesEncountered |= TokenType_t::vector;
+					}
+					else if (token->TokenType.has_flag(TokenType_t::standard))
+					{
+						tokenTypesEncountered |= TokenType_t::standard;
+					}
+				}
+				else if (token->TokenPermission.has_flag(TokenPermission_t::node))
+				{
+					if (token->TokenPermission.has_flag(TokenPermission_t::delete_))
+					{
+						tokenPermissionsEncountered |= TokenPermission_t::delete_;
+					}
+					else if (token->TokenPermission.has_flag(TokenPermission_t::ignore))
+					{
+						tokenPermissionsEncountered |= TokenPermission_t::ignore;
+					}
+					else if (token->TokenPermission.has_flag(TokenPermission_t::node))
+					{
+						tokenPermissionsEncountered |= TokenPermission_t::node;
+					}
+				}
+			}
+		}
+	}
+
+	tokenType |= tokenTypesEncountered.enum_value();             //This has only active flags if non terminals occured
+	tokenPermission |= tokenPermissionsEncountered.enum_value(); //This has active flags when non terminals occured
+	// Note mixed terminals trigger both type list
+	return tokenType.active_flags() + tokenPermission.active_flags();
 }
 
 deamer::Rule* deamer::TypeAnalyzer::CreateRule_Without_Ignored_Tokens(Rule* rule) const
 {
-    std::vector<Token*> tokens;
-    bool encountered_ignorable_token = false;
-	
-	for(Token* token : rule->Tokens)
+	std::vector<Token*> tokens;
+	bool encountered_ignorable_token = false;
+
+	for (Token* token : rule->Tokens)
 	{
-        if (!token->TokenPermission.has_flag(TokenPermission_t::ignore) && token != type_)
-            tokens.push_back(token);
+		if (!token->TokenPermission.has_flag(TokenPermission_t::ignore) && token != type_)
+			tokens.push_back(token);
 		if (token->TokenPermission.has_flag(TokenPermission_t::ignore))
-            encountered_ignorable_token = true;
+			encountered_ignorable_token = true;
 	}
 	if (encountered_ignorable_token)
-        return RuleFactory().MakeDefaultRule(tokens);
-    else
-	    return RuleFactory().MakeRule(tokens);
+		return RuleFactory().MakeDefaultRule(tokens);
+	else
+		return RuleFactory().MakeRule(tokens);
 }
 
 bool deamer::TypeAnalyzer::IsThisRuleUniqueInVector(const std::vector<Rule*>& rules, const Rule& rule) const
 {
-	for(Rule* unique_rule : rules)
+	for (Rule* unique_rule : rules)
 	{
-        if (AreRulesEqual(*unique_rule, rule))
-            return false;
+		if (AreRulesEqual(*unique_rule, rule))
+			return false;
 	}
-    return true;
+	return true;
 }
 
 bool deamer::TypeAnalyzer::AreRulesEqual(const Rule& rule1, const Rule& rule2) const
 {
-    if (rule1.Tokens.size() != rule2.Tokens.size())
-        return false;
-	
-	for(unsigned i = 0; i < rule1.Tokens.size(); i++)
+	if (rule1.Tokens.size() != rule2.Tokens.size())
+		return false;
+
+	for (unsigned i = 0; i < rule1.Tokens.size(); i++)
 	{
-        if (rule1.Tokens[i] != rule2.Tokens[i])
-            return false;
+		if (rule1.Tokens[i] != rule2.Tokens[i])
+			return false;
 	}
-    return true;
+	return true;
 }
 
 void deamer::TypeAnalyzer::AddTokenToVectorIfNotAlreadyInVector(std::vector<Token*>& tokens, Token* token) const
 {
-    for (Token* token_in_vector : tokens)
-        if (token_in_vector == token)
-            return;
-    tokens.push_back(token);
+	for (Token* token_in_vector : tokens)
+		if (token_in_vector == token)
+			return;
+	tokens.push_back(token);
 }
