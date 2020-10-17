052e65391204f538d7eff1015a83e476807b361c
Thimo Bohmer <thimo@bohmer.net>
2020-10-17 17:09:13 +0200

    Update BisonGroupedRuleFormatter
    It can now supports grouping of:
    -pure ignore node groups
    -pure node groups
    -pure type groups
    -mix between node and type groups

--- Deamer/include/Deamer/ParserGen/BisonBuilder/RuleFormatters/BisonGroupedRuleFormatter.h
+++ Deamer/include/Deamer/ParserGen/BisonBuilder/RuleFormatters/BisonGroupedRuleFormatter.h
@@ -9,14 +9,34 @@
 #ifndef DEAMER_PARSERGEN_BISONBUILDER_RULEFORMATTERS_BISONGROUPEDRULEFORMATTER_H
 #define DEAMER_PARSERGEN_BISONBUILDER_RULEFORMATTERS_BISONGROUPEDRULEFORMATTER_H
 
+#include "Deamer/LanguageGen/LanguageDefinitionDataStructures/Type.h"
 #include "Deamer/ParserGen/BisonBuilder/RuleFormatters/BisonRuleFormatter.h"
+#include "Deamer/Types/BitwiseEnum.h"
 
 namespace deamer
 {
 	class BisonGroupedRuleFormatter : public BisonRuleFormatter
 	{
 	private:
+		std::string MakeExecutedCodeForMixedGroup(const Token& currentToken) const;
+		
+		std::string MakeExecutedCodeForPureGroup(const Token& currentToken) const;
+		std::string MakeExecutedCodeForPureGroupStandard(const Token& currentToken) const;
+		std::string MakeExecutedCodeForPureGroupVector(const Token& currentToken) const;
+		std::string MakeExecutedCodeForPureGroupNode(const Token& currentToken) const;
+		std::string MakeExecutedCodeForPureGroupIgnore(const Token& currentToken) const;
+		std::string MakeExecutedCodeForPureGroupDelete(const Token& currentToken) const;
+		
+		std::string MakeNonTerminalCode(const Token& currentToken) const;
+		std::string MakeVectorCode(const Token& currentToken) const;
+		std::string MakeTerminalCode(const Token& currentToken) const;
+		std::string MakeIgnoreCodeWithoutGroupObject(const Token& currentToken) const;
+		std::string MakeIgnoreCodeWithGroupObject(const Token& currentToken) const;
+
 		std::string MakeExecutedCodeForProductionRule() const override;
+		bool isPureGroup = false; //Does it exists of 1 group?
+		BitwiseEnum<TokenType_t> groupNonTerminalTypes;    //The types occuring in our group
+		BitwiseEnum<TokenPermission_t> groupTerminalTypes; //The types occuring in our group
 	public:
 		BisonGroupedRuleFormatter(std::string& languageName, unsigned currentLineNumber, bool isFirstType, Type* currentType, Rule* currentRule);
 	};
