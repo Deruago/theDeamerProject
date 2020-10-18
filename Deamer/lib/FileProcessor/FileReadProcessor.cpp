c375e0c62e7f8e4e5ac29e86a0c12ff47bafa6f5
Thimo Bohmer <thimo@bohmer.net>
2020-10-18 20:01:35 +0200

    Update FileReadProcessor
    FileReadProcessor is now updated to correctly process
    DLDL files.

--- Deamer/lib/FileProcessor/FileReadProcessor.cpp
+++ Deamer/lib/FileProcessor/FileReadProcessor.cpp
@@ -9,9 +9,13 @@
 #include "Deamer/FileProcessor/FileReadProcessor.h"
 #include "Deamer/FileProcessor/FileReadPasses/FileReadRemoveUnusedText.h"
 #include "Deamer/FileProcessor/FileReadPasses/FileReadReplaceIndentationWithBrackets.h"
+#include "Deamer/FileProcessor/FileReadPasses/FileReadRemoveLinesStartingWithCharacter.h"
 #include "Deamer/FileProcessor/FileReadPasses/FileReadAddBracketsAtSameLevelIndentedLines.h"
+#include "Deamer/FileProcessor/FileReadPasses/FileReadReformatLines.h"
+#include "Deamer/FileProcessor/FileReadPasses/FileReadReplaceCharactersWithString.h"
 #include <fstream>
 #include <iostream>
+
 
 
 deamer::FileReadProcessor::FileReadProcessor(FileRead_Passes file_read_passes)
@@ -24,13 +28,38 @@ std::string deamer::FileReadProcessor::ReadFile(const std::string& file_location
 	std::string output;
 	std::ifstream read_in_file(file_location);
 	const FileReadRemoveUnusedText input_cleaner;
+	const FileReadRemoveLinesStartingWithCharacter comment_cleaner("/");
+
+	const FileReadReformatLines lineReformat;
+	
 	const FileReadReplaceIndentationWithBrackets indentation_to_brackets;
-	const FileReadAddBracketsAtSameLevelIndentedLines brackets_at_same_level("/", ":");
+	const FileReadAddBracketsAtSameLevelIndentedLines brackets_at_same_level("", "");
 
 	auto lines = input_cleaner.ProcessFile(read_in_file);
-	lines.push_back("/\r\n");
+	//lines.push_back("/\r\n");
+	lines.push_back("\r\n");
 	lines = indentation_to_brackets.ProcessLines(lines);
 	lines = brackets_at_same_level.ProcessLines(lines);
+
+	lines = lineReformat.ProcessLines(lines);
+
+	lines = comment_cleaner.ProcessLines(lines);
+
+	const std::string tmpLine = lines[lines.size() - 1];
+	std::string newTMPLine;
+	for (const char tmpChar : tmpLine)
+	{
+		if (tmpChar == '/')
+		{
+			newTMPLine += '\n';
+			break;
+		}
+
+		newTMPLine += tmpChar;
+	}
+
+	lines[lines.size() - 1] = newTMPLine;
+	
 	for(const std::string& line : lines)
 	{
 		output += line;
