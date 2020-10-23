0ef987544aa471b427da79df7a45da157b785fa8
Thimo Bohmer <thimo@bohmer.net>
2020-10-23 14:02:56 +0200

    Add logic to FileReadProcessor
    It now deletes comments appearing as: }/

--- Deamer/lib/FileProcessor/FileReadProcessor.cpp
+++ Deamer/lib/FileProcessor/FileReadProcessor.cpp
@@ -31,7 +31,7 @@ std::string deamer::FileReadProcessor::ReadFile(const std::string& file_location
 	const FileReadRemoveLinesStartingWithCharacter comment_cleaner("/");
 
 	const FileReadReformatLines lineReformat;
-	
+
 	const FileReadReplaceIndentationWithBrackets indentation_to_brackets;
 	const FileReadAddBracketsAtSameLevelIndentedLines brackets_at_same_level("", "");
 
@@ -45,7 +45,7 @@ std::string deamer::FileReadProcessor::ReadFile(const std::string& file_location
 
 	lines = comment_cleaner.ProcessLines(lines);
 
-	const std::string tmpLine = lines[lines.size() - 1];
+	/*const std::string tmpLine = lines[lines.size() - 1];
 	std::string newTMPLine;
 	for (const char tmpChar : tmpLine)
 	{
@@ -58,7 +58,53 @@ std::string deamer::FileReadProcessor::ReadFile(const std::string& file_location
 		newTMPLine += tmpChar;
 	}
 
-	lines[lines.size() - 1] = newTMPLine;
+	lines[lines.size() - 1] = newTMPLine;*/
+
+	// Deletes everything after comment in following situation:
+	// }/coment
+	std::vector<std::string> newLines;
+	for (unsigned i = 0; i < lines.size(); i++)
+	{
+		bool LEFT_BRACKET_FOUND = false;
+		unsigned CharCount = 0;
+		std::string newTMPLine;
+		for (const char character : lines[i])
+		{
+			CharCount++;
+			if (LEFT_BRACKET_FOUND && character == '/' && CharCount == 2)
+			{
+				newTMPLine += "\n";
+				break;
+			}
+			if (character == '}')
+				LEFT_BRACKET_FOUND = true;
+			else
+				LEFT_BRACKET_FOUND = false;
+
+			newTMPLine += character;
+		}
+		newLines.push_back(newTMPLine);
+	}
+	lines = newLines;
+
+	std::string tmpStr = lines[lines.size() - 1];
+	if (tmpStr.size() >= 4)
+	{
+		std::cout << tmpStr << std::endl;
+		if (*(tmpStr.end() - 4) == '{' &&
+			*(tmpStr.end() - 3) == '}' &&
+			*(tmpStr.end() - 2) == '/')
+		{
+			tmpStr.pop_back();
+			tmpStr.pop_back();
+			tmpStr.pop_back();
+			tmpStr.pop_back();
+			tmpStr.push_back('\n');
+		}
+		std::cout << tmpStr << std::endl;
+		lines[lines.size() - 1] = tmpStr;
+	}
+	
 	
 	for(const std::string& line : lines)
 	{
