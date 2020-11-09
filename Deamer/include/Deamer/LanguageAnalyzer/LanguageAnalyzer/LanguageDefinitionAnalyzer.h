6f4c5bd813016060933c371f92c6db9025926616
Thimo Bohmer <thimo@bohmer.net>
2020-11-09 20:27:56 +0100

    Updated LanguageDefinitionAnalyzer
    The LanguageDefinitionAnalyzer can now find out what the start token
    is of a given language definition.

--- Deamer/include/Deamer/LanguageAnalyzer/LanguageAnalyzer/LanguageDefinitionAnalyzer.h
+++ Deamer/include/Deamer/LanguageAnalyzer/LanguageAnalyzer/LanguageDefinitionAnalyzer.h
@@ -5,20 +5,25 @@
  * Modified:
  * -July 2020 Thimo Böhmer
  * -August 2020 Thimo Böhmer
+ * -November 2020 Thimo Bohmer
  */
 
 #ifndef DEAMER_LANGUAGEANALYZER_LANGUAGEANALYZER_LANGUAGEDEFINITIONANALYZER_H
 #define DEAMER_LANGUAGEANALYZER_LANGUAGEANALYZER_LANGUAGEDEFINITIONANALYZER_H
 
+#include "Deamer/LanguageGen/LanguageDefinition.h"
+
 namespace deamer
 {
 	class LanguageDefinitionAnalyzer
 	{
+	private:
+		LanguageDefinition languageDefinition;
 	public:
-		LanguageDefinitionAnalyzer() = default;
+		LanguageDefinitionAnalyzer(const LanguageDefinition& languageDefinition_);
 		~LanguageDefinitionAnalyzer() = default;
 
-		
+		Token* GetStartToken() const;
 	};
 }
 
--- Deamer/lib/LanguageAnalyzer/LanguageAnalyzer/LanguageDefinitionAnalyzer.cpp
+++ Deamer/lib/LanguageAnalyzer/LanguageAnalyzer/LanguageDefinitionAnalyzer.cpp
@@ -7,3 +7,19 @@
  */
 
 #include "Deamer/LanguageAnalyzer/LanguageAnalyzer/LanguageDefinitionAnalyzer.h"
+
+deamer::LanguageDefinitionAnalyzer::LanguageDefinitionAnalyzer(const LanguageDefinition& languageDefinition_)
+{
+	languageDefinition = languageDefinition_;
+}
+
+deamer::Token* deamer::LanguageDefinitionAnalyzer::GetStartToken() const
+{
+	if (languageDefinition.StartType != nullptr)
+		return languageDefinition.StartType;
+	else
+		if (languageDefinition.Types.empty())
+			return nullptr;
+		else
+			return languageDefinition.Types[0];
+}
