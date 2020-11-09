60e5541232454f4c30e0f49938b405106ce4a70f
Thimo Bohmer <thimo@bohmer.net>
2020-11-09 20:23:11 +0100

    Add ColorType and TextFont enum

---
+++ Deamer/include/Deamer/Types/Printer/ColorType.h
@@ -0,0 +1,38 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Bohmer 2020
+ *
+ * Modified:
+ * -November 2020 Thimo Bohmer
+ */
+
+#ifndef DEAMER_TYPES_PRINTER_COLORTYPE_H
+#define DEAMER_TYPES_PRINTER_COLORTYPE_H
+
+#include "Deamer/Types/Printer/ColorType.h"
+
+namespace deamer { namespace types {
+
+	enum class ColorType
+	{
+		Black,
+		Red,
+		Green,
+		Yellow,
+		Blue,
+		Magenta,
+		Cyan,
+		White,
+		BrightBlack,
+		BrightRed,
+		BrightGreen,
+		BrightYellow,
+		BrightBlue,
+		BrightMagenta,
+		BrightCyan,
+		BrightWhite,
+	};
+	
+}}
+
+#endif //DEAMER_TYPES_PRINTER_COLORTYPE_H
---
+++ Deamer/include/Deamer/Types/Printer/TextFont.h
@@ -0,0 +1,25 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Bohmer 2020
+ *
+ * Modified:
+ * -November 2020 Thimo Bohmer
+ */
+
+#ifndef DEAMER_TYPES_PRINTER_TEXTFONT_H
+#define DEAMER_TYPES_PRINTER_TEXTFONT_H
+
+#include "Deamer/Types/Printer/ColorType.h"
+
+namespace deamer { namespace types {
+
+		enum class TextFont
+		{
+			Bolt,
+			Italic,
+			Standard,
+		};
+
+}}
+
+#endif //DEAMER_TYPES_PRINTER_TEXTFONT_H
