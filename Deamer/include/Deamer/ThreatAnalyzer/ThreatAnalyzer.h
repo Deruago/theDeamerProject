5c04a32fa5c3273d24882efc57bca47b79d00e6d
Thimo Bohmer <thimo@bohmer.net>
2020-11-09 20:26:42 +0100

    Add ThreatAnalyzer abstract class
    This class is the base class for all threatanalyzers.
    
    A threatanalyzer is an analyzer that analyzes the languagedefinition on
    potential threats. These threats are shows as compiler errors and warnings.

---
+++ Deamer/include/Deamer/ThreatAnalyzer/ThreatAnalyzer.h
@@ -0,0 +1,33 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Bohmer 2020
+ *
+ * Modified:
+ * -November 2020 Thimo Bohmer
+ */
+
+#ifndef DEAMER_THREATANALYZER_THREATANALYZER_H
+#define DEAMER_THREATANALYZER_THREATANALYZER_H
+
+#include "Deamer/ThreatAnalyzer/ThreatData/ThreatData.h"
+#include <vector>
+
+namespace deamer {
+	class LanguageDefinition;
+}
+
+namespace deamer { namespace threat {
+
+	class ThreatAnalyzer
+	{
+	private:
+	public:
+		ThreatAnalyzer() = default;
+		virtual ~ThreatAnalyzer() = default;
+
+		virtual std::vector<ThreatData> AnalyseLanguageDefinition(const LanguageDefinition& languageDefinition) const = 0;
+	};
+
+}}
+
+#endif //DEAMER_THREATANALYZER_THREATANALYZER_H
---
+++ Deamer/lib/ThreatAnalyzer/ThreatAnalyzer.cpp
@@ -0,0 +1,9 @@
+/*
+ * Part of the Deamer Project, under the GPLV3 license.
+ * Copyright Thimo Bohmer 2020
+ *
+ * Modified:
+ * -November 2020 Thimo Bohmer
+ */
+
+#include "Deamer/ThreatAnalyzer/ThreatAnalyzer.h"
