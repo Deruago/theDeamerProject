eef0a66688c026982f6a784eac8e4ec1abc454b4
Thimo Bohmer <thimo@bohmer.net>
2021-02-14 16:36:41 +0100

    Replaced old makefile system with new cmake system
    cmake now covers the following:
    -installing the project
    -building the project
    -running unit tests on the project
    
    This commit also adds some test unit tests

---
+++ Deamer/tests/test2.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+TEST(test1222, test1_212true_is_true)
+{
+	EXPECT_TRUE(true);
+	EXPECT_TRUE(true);
+	EXPECT_TRUE(true);
+	// EXPECT_TRUE(false); // uncheck if you want to test errors in testing
+	EXPECT_TRUE(true);
+}
