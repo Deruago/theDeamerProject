da157e3384c4c0b40184a670fa5b3d90869d74a4
Thimo Bohmer <thimo@bohmer.net>
2021-06-21 18:33:54 +0200

    Extended DST example
    DST example now uses colorization LPD

--- Deamer/examples/DST/Definition/Settings/CompilerGenerator.h
+++ Deamer/examples/DST/Definition/Settings/CompilerGenerator.h
@@ -5,6 +5,7 @@
 #include "Deamer/Compiler/Generator/Compiler.h"
 #include "Deamer/Lexer/Generator/Flex/Flex.h"
 #include "Deamer/Parser/Generator/Bison/Bison.h"
+#include "Deamer/Tool/Type/SyntaxHighlighter/UDL/UDL.h"
 #include "Language.h"
 
 namespace DST::setting
@@ -23,10 +24,12 @@ namespace DST::setting
 			deamer::lexer::generator::flex::Flex flex(GetLanguageDefinition());
 			deamer::parser::generator::bison::Bison bison(GetLanguageDefinition());
 			deamer::ast::generation::cpp::CPP ast(GetLanguageDefinition());
+			deamer::tool::type::syntaxhighlighter::udl::UDL udl(GetLanguageDefinition());
 
 			output.AddLanguageToolOutput(flex.Generate());
 			output.AddLanguageToolOutput(bison.Generate());
 			output.AddLanguageToolOutput(ast.Generate());
+			output.AddLanguageToolOutput(udl.Generate());
 
 			return output;
 		}
--- Deamer/examples/DST/Definition/Settings/Language.h
+++ Deamer/examples/DST/Definition/Settings/Language.h
@@ -1,6 +1,7 @@
 #ifndef DST_SETTING_DEFINITION_LANGUAGE_H
 #define DST_SETTING_DEFINITION_LANGUAGE_H
 
+#include "Colorization.h"
 #include "Deamer/Language/Generator/Definition/Language.h"
 #include "Generation.h"
 #include "Grammar.h"
@@ -9,20 +10,23 @@
 
 namespace DST::setting
 {
-	class Language : public ::deamer::language::generator::definition::Language<
-						 ::DST::setting::Language, ::DST::setting::Lexicon, ::DST::setting::Grammar,
-						 ::DST::setting::Identity, ::DST::setting::Generation>,
-					 public ::DST::setting::Lexicon,
-					 public ::DST::setting::Grammar,
-					 public ::DST::setting::Identity,
-					 public ::DST::setting::Generation
+	class Language
+		: public ::deamer::language::generator::definition::Language<
+			  ::DST::setting::Language, ::DST::setting::Lexicon, ::DST::setting::Grammar,
+			  ::DST::setting::Identity, ::DST::setting::Generation, ::DST::setting::Colorization>,
+		  public ::DST::setting::Lexicon,
+		  public ::DST::setting::Grammar,
+		  public ::DST::setting::Identity,
+		  public ::DST::setting::Generation,
+		  public ::DST::setting::Colorization
 	{
 	public:
 		Language()
 			: ::DST::setting::Lexicon(this),
 			  ::DST::setting::Grammar(this),
 			  ::DST::setting::Identity(this),
-			  ::DST::setting::Generation(this)
+			  ::DST::setting::Generation(this),
+			  ::DST::setting::Colorization(this)
 		{
 		}
 		~Language() override = default;
