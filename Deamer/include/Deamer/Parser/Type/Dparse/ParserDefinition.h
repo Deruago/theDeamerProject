117657674510643de3f69c64bd96e9150e12811a
Thimo Bohmer <thimo@bohmer.net>
2022-04-17 15:56:10 +0200

    Implement Dparse ParserDefinition generation
    Given some StateField this class generates the parser implementation.

---
+++ Deamer/include/Deamer/Parser/Type/Dparse/ParserDefinition.h
@@ -0,0 +1,48 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+/*
+ * Part of the DeamerProject.
+ * For more information go to: https://github.com/Deruago/theDeamerProject
+ */
+
+#ifndef DEAMER_PARSER_TYPE_DPARSE_PARSERDEFINITION_H
+#define DEAMER_PARSER_TYPE_DPARSE_PARSERDEFINITION_H
+
+#include "Deamer/File/Tool/Data.h"
+#include "Deamer/Language/Analyzer/Analyzer.h"
+#include "Deamer/Parser/Generator/Dparse/Dparse.h"
+#include "Deamer/Parser/Type/Dparse/StateField.h"
+
+namespace deamer::parser::type::dparse
+{
+	class ParserDefinition : public file::tool::Data
+	{
+	private:
+		const generator::dparse::Dparse::ReferenceType reference;
+		const std::string name;
+		const generator::dparse::StateField& stateField;
+
+	public:
+		ParserDefinition(const generator::dparse::Dparse::ReferenceType reference_,
+						 const generator::dparse::StateField& stateField_);
+		virtual ~ParserDefinition() override = default;
+
+	public:
+		std::string Generate() const override;
+	};
+}
+
+#endif // DEAMER_PARSER_TYPE_DPARSE_PARSERDEFINITION_H
