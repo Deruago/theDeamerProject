66a7b38ed7789a4c4907b196476a2aeddb40bd72
Thimo Bohmer <thimo@bohmer.net>
2022-04-24 18:01:49 +0200

    Update Github Workflow

--- installer.py
+++ installer.py
@@ -6,17 +6,26 @@ import subprocess
 subfolders = [ f.path for f in os.scandir("./") if f.is_dir() ]
 
 # installation groups
-# Deamer, DeamerExternal, DeamerAlgorithm, DLDL
+# Deamer, DLDL
 install_deamer_core = True
+
+# DeamerExternal, DeamerAlgorithm
+install_deamer_external = True
 
 # DST
 install_deamer_optional = False
 
+confirm_with_enter = True
+
 build_dir = "build"
 
 def install_header():
+    global confirm_with_enter
+    
     print("This will automatically install the several subprojects.")
-    input("Press enter to continue this process.")
+    
+    if confirm_with_enter:
+        input("Press enter to continue this process.")
 
 
 def make_directory(name: str):
@@ -30,47 +39,68 @@ def subdirectory_exists(name: str):
     return f"./{name}" in subfolders
 
 
-def install_project(name: str, install: bool):
+def install_project(name: str, install: bool, test_argument: str, extra_args: str = ""):
+    # Test argument specifies the option to enable/disable testing
+    if test_argument is not None:
+        test_argument = f"-D{test_argument}=OFF"
+        
     if install and subdirectory_exists(name):
         print(f"Installing {name}")
         
         make_directory(f"{build_dir}/{name}")
-        os.system(f'cd {build_dir}/{name} && cmake ../../{name} -DINSTALL_GTEST=off && cmake --build . --target install')
+        os.system(f'cd {build_dir}/{name} && cmake ../../{name} -DINSTALL_GTEST=OFF {test_argument} {extra_args} && cmake --build . --target install')
         
         print(f"Done installing {name}")
  
 
 def install_subprojects():
+    global install_deamer_core
+    global install_deamer_external
+    global install_deamer_optional
+    
     print(subfolders)
     
     # Deamer External and Deamer Algorithm are dependency-less, and are used by
     # most subprojects. Thus should be installed first
-    install_project("DeamerExternal", install_deamer_core)
-    install_project("DeamerAlgorithm", install_deamer_core)
+    install_project("DeamerExternal", install_deamer_external, "DEAMER_EXTERNAL_BUILD_TESTS")
+    install_project("DeamerAlgorithm", install_deamer_external, "DEAMER_ALGORITHM_BUILD_TESTS")
     
     # Dregx Depends on DeamerExternal
     # Deamer uses Dregx for DFA Construction and Regex Fuzzing
-    install_project("Dregx", install_deamer_core)
+    install_project("DREGX", install_deamer_external, None, "-Ddregx_ENABLE_COMPILER_GENERATOR=OFF")
     
     # Required
-    install_project("Deamer", install_deamer_core)
-    install_project("DLDL", install_deamer_core)
+    install_project("Deamer", install_deamer_core, "DEAMER_BUILD_TESTS")
+    install_project("DLDL", install_deamer_core, "DLDL_BUILD_TESTS")
     
     # Optional
-    install_project("DST", install_deamer_optional)
+    install_project("DST", install_deamer_optional, "TESTS")
 
 def handle_arguments():
     global install_deamer_core
+    global install_deamer_external
     global install_deamer_optional
+    global confirm_with_enter
 
     if len(sys.argv) <= 1:
         return
     
     for argument in sys.argv:
+        if argument == "-no-confirm-with-enter":
+            confirm_with_enter = False
+    
         if argument == "-complete":
             print("Installing everything available.")
             install_deamer_core = True
+            install_deamer_external = True
             install_deamer_optional = True
+        
+        if argument == "-external-only":
+            print("Installing only external required depencencies.")
+            install_deamer_core = False
+            install_deamer_external = True
+            install_deamer_optional = False
+            
         if argument == "-optional":
             print("Also installing the optional parts")
             install_deamer_optional = True
