50b8a9b72815709c6bc569cab46f52d810954310
Thimo Bohmer <thimo@bohmer.net>
2022-06-22 21:16:43 +0200

    Installer now supports new features
    New features include:
    - Local install (still requires further improvements in the projects to support proper local installation)
    - Support for Deamer Package installation.
    
    Deamer package installation is done by going to the repository, cloning it, and then installing it.

--- installer.py
+++ installer.py
@@ -15,9 +15,20 @@ install_deamer_external = True
 # DST
 install_deamer_optional = False
 
+# FGL (This is required for some tooling)
+install_deamer_extra = False
+
 confirm_with_enter = True
 
 build_dir = "build"
+
+install_location_include = None
+install_location_lib = None
+install_location_bin = None
+install_location_include_argument = ""
+install_location_lib_argument = ""
+install_location_bin_argument = ""
+install_prefix_argument = ""
 
 def install_header():
     global confirm_with_enter
@@ -38,23 +49,93 @@ def make_directory(name: str):
 def subdirectory_exists(name: str):
     return f"./{name}" in subfolders
 
+def setup_install_locations():
+    global install_location_include
+    global install_location_lib
+    global install_location_bin
+    global install_location_include_argument
+    global install_location_lib_argument
+    global install_location_bin_argument
+    global install_prefix_argument
+    
+    install_is_relative = False
+    if install_location_include is not None:
+        install_location_include_argument = f'-DCMAKE_INSTALL_INCLUDEDIR="{install_location_include}"'
+        install_is_relative = True
+        
+    if install_location_lib is not None:
+        install_location_lib_argument = f'-DCMAKE_INSTALL_LIBDIR="{install_location_lib}"'
+        install_is_relative = True
+        
+    if install_location_bin is not None:
+        install_location_bin_argument = f'-DCMAKE_INSTALL_BINDIR="{install_location_bin}"'
+        install_is_relative = True
+    
+    if install_is_relative:
+        install_prefix_argument = '-DCMAKE_INSTALL_PREFIX="../../install/"'
 
-def install_project(name: str, install: bool, test_argument: str, extra_args: str = ""):
+# Installs a Deamer Package
+# If location is true it will check in the Deamer-lang github repository
+# Otherwise it will look at the repositories of @Deruago
+def install_extra_deamer_package(name: str, install: bool, test_argument: str = None, extra_args: str = "", location: bool = True):
+    global install_location_lib_argument
+    global install_location_bin_argument
+    global install_prefix_argument
+    setup_install_locations()
+    
     # Test argument specifies the option to enable/disable testing
     if test_argument is not None:
         test_argument = f"-D{test_argument}=OFF"
     else:
         test_argument = ""
+    
+    if install:
+        print(f"Retrieving Deamer Package: {name}")
         
+        if location:
+            os.system(f"git clone https://github.com/deamer-lang/{name}/ {build_dir}/DeamerPackage/{name}")
+        else:
+            os.system(f"git clone https://github.com/Deruago/{name}/ {build_dir}/DeamerPackage/{name}")
+        
+        print(f"Done Retrieving Deamer Package: {name}")
+    
+        print(f"Installing {name}")
+        
+        make_directory(f"{build_dir}/DeamerPackage/{name}/build")
+        os.system(f'cd {build_dir}/DeamerPackage/{name}/build && cmake ../ -DINSTALL_GTEST=OFF {install_location_lib_argument} {install_location_bin_argument} {install_prefix_argument} {test_argument} {extra_args} && cmake --build . --target install')
+        
+        print(f"Done installing {name}")
+        
+# Installs a project
+def install_project(name: str, install: bool, test_argument: str = None, extra_args: str = ""):
+    global install_location_lib_argument
+    global install_location_bin_argument
+    global install_prefix_argument
+    setup_install_locations()
+    
+    # Test argument specifies the option to enable/disable testing
+    if test_argument is not None:
+        test_argument = f"-D{test_argument}=OFF"
+    else:
+        test_argument = ""
+    
+    
     if install and subdirectory_exists(name):
         print(f"Installing {name}")
         
         make_directory(f"{build_dir}/{name}")
-        os.system(f'cd {build_dir}/{name} && cmake ../../{name} -DINSTALL_GTEST=OFF {test_argument} {extra_args} && cmake --build . --target install')
+        os.system(f'cd {build_dir}/{name} && cmake ../../{name} -DINSTALL_GTEST=OFF {install_location_lib_argument} {install_location_bin_argument} {install_prefix_argument} {test_argument} {extra_args} && cmake --build . --target install')
         
         print(f"Done installing {name}")
 
+# Installs a language project
 def install_language_project(name: str, language_folder: str, install: bool, test_argument: str, extra_args: str = ""):
+    global install_location_include_argument
+    global install_location_lib_argument
+    global install_location_bin_argument
+    global install_prefix_argument
+    setup_install_locations()
+    
     # Test argument specifies the option to enable/disable testing
     if test_argument is not None:
         test_argument = f"-D{test_argument}=OFF"
@@ -65,7 +146,7 @@ def install_language_project(name: str, language_folder: str, install: bool, tes
         print(f"Installing {name}")
         
         make_directory(f"{build_dir}/{name}")
-        os.system(f'cd {build_dir}/{name} && cmake ../../{name}/{language_folder} -DINSTALL_GTEST=OFF {test_argument} {extra_args} && cmake --build . --target install')
+        os.system(f'cd {build_dir}/{name} && cmake ../../{name}/{language_folder} -DINSTALL_GTEST=OFF {install_location_include_argument} {install_location_lib_argument} {install_location_bin_argument} {install_prefix_argument} {test_argument} {test_argument} {extra_args} && cmake --build . --target install')
         
         print(f"Done installing {name}")
  
@@ -74,6 +155,7 @@ def install_subprojects():
     global install_deamer_core
     global install_deamer_external
     global install_deamer_optional
+    global install_deamer_extra
     
     print(subfolders)
     
@@ -91,14 +173,21 @@ def install_subprojects():
     install_language_project("DLDL", "DLDL", install_deamer_core, "DLDL_BUILD_TESTS")
     
     # Optional
-    install_project("DST", install_deamer_optional, "TESTS")
+    install_project("DST", install_deamer_extra)
+    
+    install_extra_deamer_package("FGL", install_deamer_optional, "TESTS", location=False)
 
 def handle_arguments():
     global install_deamer_core
     global install_deamer_external
     global install_deamer_optional
+    global install_deamer_extra
+    
     global confirm_with_enter
-
+    global install_location_include
+    global install_location_lib
+    global install_location_bin
+    
     if len(sys.argv) <= 1:
         return
     
@@ -111,6 +200,13 @@ def handle_arguments():
             install_deamer_core = True
             install_deamer_external = True
             install_deamer_optional = True
+            
+        if argument == "-extra":
+            print("Installing everything available.")
+            install_deamer_core = True
+            install_deamer_external = True
+            install_deamer_optional = True
+            install_deamer_extra = True
         
         if argument == "-external-only":
             print("Installing only external required depencencies.")
@@ -121,6 +217,11 @@ def handle_arguments():
         if argument == "-optional":
             print("Also installing the optional parts")
             install_deamer_optional = True
+        
+        if argument == "-local-install":
+            install_location_include = "include/"
+            install_location_lib = "lib/"
+            install_location_bin = "bin/"
             
 def handle_build_directory():
     global build_dir
