662d77e98cd779dcc3f8ec67e6512bbf63e79423
Thimo Bohmer <thimo@bohmer.net>
2023-04-30 16:13:09 +0200

    Add initial start of Argument LPD
    This LPD is used for modelling the compiler arguments for a compiler.

---
+++ Deamer/include/Deamer/Language/Generator/Definition/Property/User/Main/Argument.h
@@ -0,0 +1,57 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+ /*
+  * Part of the DeamerProject.
+  * For more information go to: https://github.com/Deruago/theDeamerProject
+  */
+
+#ifndef DEAMER_LANGUAGE_GENERATOR_DEFINITION_PROPERTY_USER_MAIN_ARGUMENT_H
+#define DEAMER_LANGUAGE_GENERATOR_DEFINITION_PROPERTY_USER_MAIN_ARGUMENT_H
+
+#include "Deamer/Language/Generator/Definition/Property/User/Base.h"
+#include "Deamer/Language/Type/Definition/Property/Main/Argument.h"
+
+namespace deamer::language::generator::definition::property::user
+{
+	/*!	\class Argument
+	 *
+	 *	\brief Used to generate a Argument definition.
+	 *
+	 *	\tparam LanguageDefinitionType The language the Argument is made for.
+	 */
+	template<typename LanguageDefinitionType>
+	class Argument : public Base<LanguageDefinitionType, type::definition::property::main::Argument>
+	{
+	private:
+	public:
+		Argument(LanguageDefinitionType* language_)
+			: Base<LanguageDefinitionType, type::definition::property::main::Argument>(language_)
+		{
+		}
+
+		/*! \fn Generate
+		 *
+		 *	\brief Generates Argument property definition.
+		 */
+		void GenerateDefinition() override
+		{
+
+			this->generatedDefinition = new type::definition::property::main::Argument();
+		}
+	};
+}
+
+#endif // DEAMER_LANGUAGE_GENERATOR_DEFINITION_PROPERTY_USER_MAIN_ARGUMENT_H
---
+++ Deamer/include/Deamer/Language/Type/Definition/Property/Main/Argument.h
@@ -0,0 +1,62 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+/*
+ * Part of the DeamerProject.
+ * For more information go to: https://github.com/Deruago/theDeamerProject
+ */
+
+#ifndef DEAMER_LANGUAGE_DATASTRUCTURE_DEFINITION_PROPERTY_MAIN_ARGUMENT_H
+#define DEAMER_LANGUAGE_DATASTRUCTURE_DEFINITION_PROPERTY_MAIN_ARGUMENT_H
+
+
+#include "Deamer/Language/Type/Definition/Property/Main/Definition.h"
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace deamer::language::type::definition::property::main
+{
+	/*! \class Argument
+	 *
+	 *	\brief 
+	 *	Language Property Definition for Language Arguments.
+	 *	This is used to generate Arguments for your language or tooling around your language.
+	 *	
+	 *	Designed by Thimo Böhmer
+
+	 *	\details 
+
+	 */
+	class Argument final : public Definition
+	{
+	public:
+
+	
+	public:
+		Argument();
+		virtual ~Argument() = default;
+
+	public:
+
+
+	public:
+		
+
+	};
+}
+
+#endif // DEAMER_LANGUAGE_DATASTRUCTURE_DEFINITION_PROPERTY_MAIN_ARGUMENT_H
