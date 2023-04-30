f7c31f25a8da11137aceeb17d8d801ead959c087
Thimo Bohmer <thimo@bohmer.net>
2023-04-30 16:11:37 +0200

    Implemented Comment LPD
    Comment LPD is used to model the comment model used in languages.

---
+++ Deamer/lib/Language/Type/Definition/Object/Main/Comment/CommentValue.cpp
@@ -0,0 +1,52 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+/*
+ * Part of the DeamerProject.
+ * For more information go to: https://github.com/Deruago/theDeamerProject
+ */
+
+#include "Deamer/Language/Type/Definition/Object/Main/Comment/CommentValue.h"
+
+#include <utility>
+
+deamer::language::type::definition::object::main::CommentValue::CommentValue(object::main::Terminal* syntacticRelation_, 
+			std::string startPattern_, 
+			std::string escapeSequence_, 
+			std::string endPattern_)
+	: Base(Type::CommentValue),
+	syntacticRelation(syntacticRelation_),
+	startPattern(startPattern_),
+	escapeSequence(escapeSequence_),
+	endPattern(endPattern_)
+{
+	references.Add(syntacticRelation);
+
+}
+
+bool deamer::language::type::definition::object::main::CommentValue::operator==(const CommentValue& rhs) const noexcept
+{
+	return this == &rhs || (this->syntacticRelation == rhs.syntacticRelation && this->startPattern == rhs.startPattern && this->escapeSequence == rhs.escapeSequence && this->endPattern == rhs.endPattern && true);
+}
+
+
+deamer::language::type::definition::object::main::CommentValue::CommentValue() : CommentValue(nullptr, 
+			"", 
+			"", 
+			"")
+{
+}
+
+
